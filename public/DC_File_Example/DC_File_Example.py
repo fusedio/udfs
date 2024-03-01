a5a5d6c8fe7b1a3e91d6de4d7bf168784b68ea4a
kashuk <sina@nyu.edu>
2024-02-29 22:03:47 -0500

    add DC file example

---
+++ public/DC_File_Example/DC_File_Example.py
@@ -0,0 +1,6 @@
+@fused.udf
+def udf(url='https://www2.census.gov/geo/tiger/TIGER_RD18/STATE/11_DISTRICT_OF_COLUMBIA/11/tl_rd22_11_bg.zip'):    
+    import geopandas as gpd
+    df = gpd.read_file(url)  
+    # df.geometry = df.geometry.buffer(0.001) 
+    return df
