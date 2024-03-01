cb12f18d905ef37068685b2df3506d11f2399cd1
kashuk <sina@nyu.edu>
2024-02-29 19:44:12 -0500

    update cdls to use common

--- public/CDLs_Tile_Example/CDLs_Tile_Example.py
+++ public/CDLs_Tile_Example/CDLs_Tile_Example.py
@@ -1,6 +1,7 @@
 @fused.udf
 def udf(bbox, year='2022', crop_type='', chip_len=256):
-    from utils import read_tiff, filter_crops, crop_counts
+    read_tiff = fused.core.import_from_github('https://github.com/fusedio/udfs/tree/a63664f4a4451d07efd003e318a1413c51a54889/public/common').utils.read_tiff
+    from utils import filter_crops, crop_counts
     import numpy as np 
     input_tiff_path=f"s3://fused-asset/data/cdls/{year}_30m_cdls.tif"
     arr, color_map = read_tiff(bbox, input_tiff_path, output_shape=(chip_len,chip_len), return_colormap=True)
