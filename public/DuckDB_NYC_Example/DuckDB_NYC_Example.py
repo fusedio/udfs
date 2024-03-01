0f0dc309542f8edc7d3e053bef03c6e31a327733
kashuk <sina@nyu.edu>
2024-02-29 20:06:22 -0500

    update geo_convert

--- public/DuckDB_NYC_Example/DuckDB_NYC_Example.py
+++ public/DuckDB_NYC_Example/DuckDB_NYC_Example.py
@@ -2,6 +2,7 @@
 @fused.udf
 def udf(bbox=None, agg_factor=3, min_count=5):
     import fused
+    geo_convert = fused.core.import_from_github('https://github.com/fusedio/udfs/tree/ccbab4334b0cfa989c0af7d2393fb3d607a04eef/public/common').utils.geo_convert
     import duckdb
     con = duckdb.connect()
     print('duckdb version:', duckdb.__version__)
@@ -19,5 +20,5 @@ def udf(bbox=None, agg_factor=3, min_count=5):
     HAVING cnt>{min_count} and lat>40 and lat<41
     """).df() 
     print('number of trips:', df.cnt.sum())
-    gdf = fused.utils.geo_convert(df)
+    gdf = geo_convert(df)
     return gdf
