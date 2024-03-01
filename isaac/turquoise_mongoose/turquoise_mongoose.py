752eb8e914d336082a70b8ac04c35fc872955bba
Isaac Brodsky <isaac@isaacbrodsky.com>
2024-02-29 16:16:51 -0800

    add test UDF

---
+++ isaac/turquoise_mongoose/turquoise_mongoose.py
@@ -0,0 +1,15 @@
+@fused.udf
+def udf(bbox=None, n=10):
+    import geopandas as gpd
+    import shapely
+
+    default_bbox = shapely.box(-122.549, 37.681, -122.341, 37.818)
+    tile_bbox = bbox.geometry[0] if bbox is not None else default_bbox
+    geoms = [shapely.affinity.rotate(tile_bbox, 3 * i) for i in range(n)]
+
+    return gpd.GeoDataFrame({
+      'n': list(range(n)),
+      'r': [i * 30 for i in range(n)],
+      'g': [i * 30 for i in range(n)],
+      'b': [i * 30 for i in range(n)],
+    }, geometry=geoms)
