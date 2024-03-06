2fc1ef97b633cd7619376a3a6cd27d3a85e7ba52
Isaac Brodsky <isaac@isaacbrodsky.com>
2024-03-06 12:06:29 -0800

    reformat

--- public/Overture_Maps_Example/Overture_Maps_Example.py
+++ public/Overture_Maps_Example/Overture_Maps_Example.py
@@ -1,28 +1,30 @@
 @fused.udf
-def udf(bbox,
-        release="2024-02-15-alpha.0",
-        theme="buildings",
-        type=None,
-        use_columns=None,
-        num_parts=None,
-        min_zoom=None,
+def udf(
+    bbox,
+    release="2024-02-15-alpha.0",
+    theme="buildings",
+    type=None,
+    use_columns=None,
+    num_parts=None,
+    min_zoom=None,
 ):
     import json
     import pandas as pd
     import concurrent.futures
+
     utils = fused.load(
         "https://github.com/fusedio/udfs/tree/f928ee1/public/common/"
     ).utils
 
     if use_columns:
         use_columns = json.loads(use_columns)
-    
+
     if num_parts:
         # Override the num_parts detection logic (shouldn't be needed)
         num_parts = int(num_parts)
     else:
         num_parts = 1 if theme != "buildings" else 5
-    
+
     if min_zoom:
         min_zoom = int(min_zoom)
     elif theme == "admins":
@@ -41,17 +43,16 @@ def udf(bbox,
     }
     if not type:
         type = default_type_per_theme[theme]
-    
+
     table_path = f"s3://fused-asset/overture/{release}/theme={theme}/type={type}"
     table_path = table_path.rstrip("/")
-    
+
     def get_part(part):
         part_path = f"{table_path}/part={part}/" if num_parts != 1 else table_path
         try:
-            return utils.table_to_tile(bbox,
-                                       table=part_path,
-                                       use_columns=use_columns,
-                                       min_zoom=min_zoom)
+            return utils.table_to_tile(
+                bbox, table=part_path, use_columns=use_columns, min_zoom=min_zoom
+            )
         except ValueError:
             return None
 
@@ -63,7 +64,7 @@ def udf(bbox,
         dfs = [get_part(0)]
 
     dfs = [df for df in dfs if df is not None]
-    
+
     if len(dfs):
         df = pd.concat(dfs)
         print(df.columns)
