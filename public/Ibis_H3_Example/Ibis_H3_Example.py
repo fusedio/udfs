8d2aede434242e4a65258d89abf8cf987688ef4b
samlalwani <sameerlalwani@gmail.com>
2024-04-24 11:10:38 -0700

    Ibis_H3 Example

---
+++ public/Ibis_H3_Example/Ibis_H3_Example.py
@@ -0,0 +1,71 @@
+# Note: This UDF is only for demo purposes. You may get `HTTP GET error` after several times calling it. This is the data retrieval issue caused by Cloudfront servers not responding.
+# Note: This UDF is a copy of the DuckDB_H3_Example
+# Modified to use IBIS project. All the original code is kept as is, commented out.
+
+@fused.udf
+def udf(bbox=None, resolution: int = 9, min_count: int = 10):
+    import shapely
+    import geopandas as gpd
+    import duckdb
+    import ibis
+    from ibis import _
+    from utils import load_h3_duckdb
+
+    # DuckDB is only used to download extension
+    utils = fused.load(
+        "https://github.com/fusedio/udfs/tree/f928ee1/public/common/"
+    ).utils
+
+    con = duckdb.connect(config={'allow_unsigned_extensions': True})
+    load_h3_duckdb(con)
+
+    # We use the duckdb extension h3ext
+    # con.sql(f"""INSTALL httpfs; LOAD httpfs;""")
+    con_ibis = ibis.duckdb.connect(temp_directory='/tmp', allow_unsigned_extensions=True, extensions=['h3ext'])
+
+    @fused.cache
+    def read_data(url, resolution, min_count):
+        # df = con.sql("""
+        # SELECT h3_h3_to_string(h3_latlng_to_cell(pickup_latitude, pickup_longitude, $resolution)) cell_id,
+        #        h3_cell_to_boundary_wkt(cell_id) boundary,
+        #        count(1) cnt
+        # FROM read_parquet($url)
+        # GROUP BY cell_id
+        # HAVING cnt>$min_count
+        # """, params={'url': url, 'resolution': resolution, 'min_count': min_count}).df()
+
+        sql_str_h3 = f"""
+                    SELECT 
+                    *, 
+                    h3_latlng_to_cell(pickup_latitude, pickup_longitude, {resolution}) AS cell_id,                    
+                    FROM tripdata_orig
+                    """
+        sql_str_boundary = f"""
+                            SELECT
+                            *,
+                            h3_cell_to_boundary_wkt(h3_h3_to_string(cell_id)) AS geometry
+                            FROM tripdata_h3
+                            """
+
+        gdf = (con_ibis
+               .read_parquet(url, table_name='tripdata_orig')
+               .sql(sql_str_h3)
+               .group_by(by=['cell_id'])
+               .agg(cnt=_.cell_id.count())
+               .filter(_.cnt > min_count)
+               .alias('tripdata_h3')
+               .sql(sql_str_boundary)
+               .to_pandas()
+               )
+        gdf = gpd.GeoDataFrame(gdf, geometry=gpd.GeoSeries.from_wkt(gdf.geometry), crs='epsg:4326')
+
+        return gdf
+
+    url = r'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2010-01.parquet'
+
+    gdf = read_data(url, resolution, min_count)
+
+    print("number of trips using ibis:", gdf.cnt.sum())
+    print(gdf)
+
+    return gdf
---
+++ public/Ibis_H3_Example/utils.py
@@ -0,0 +1,36 @@
+import fused
+import gzip
+
+def load_h3_duckdb(con, *, cache_ungzipped_path = True):
+    import duckdb
+    import platform
+
+    con.sql("SET home_directory='/tmp';")
+    system = platform.system()
+    arch = platform.machine()
+    arch = 'amd64' if arch == 'x86_64' else arch
+
+    detected_os = f'osx_{arch}' if system == 'Darwin' else f'linux_{arch}'
+    if detected_os == 'linux_amd64':
+        detected_os = 'linux_amd64_gcc4'
+    url = f'https://pub-cc26a6fd5d8240078bd0c2e0623393a5.r2.dev/v{duckdb.__version__}/{detected_os}/h3ext.duckdb_extension.gz'
+    # Note this is not the correct file name, it will be fixed later in this function
+    # This workaround of downloading in Python is needed because DuckDB cannot load extensions
+    # from https (ssl, secure) URLs.
+    file_name = 'h3ext_downloaded.duckdb_extension.gz'
+    downloaded_path = fused.download(url, file_name)
+
+    def ungzip_ext(url, from_path, to_path):
+        with gzip.open(from_path, 'rb') as fin:
+            with open(to_path, 'wb') as fout:
+                buf = fin.read()
+                fout.write(buf)
+        print('ungzipped H3 extension', url, from_path, to_path)
+
+    # The name of this file MUST be h3ext.duckdb_extension, or else DuckDB will not install it in the right place
+    ungzip_path = fused.core.create_path('h3ext.duckdb_extension')
+    ungzip_ext_fn = fused.cache(ungzip_ext) if cache_ungzipped_path else ungzip_ext
+    ungzip_ext_fn(url, downloaded_path, ungzip_path)
+    
+    con.sql(f"""INSTALL '{ungzip_path}';
+    LOAD h3ext;""")
