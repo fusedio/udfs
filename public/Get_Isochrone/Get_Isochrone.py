c35a56c25517054677301837180ecbb8a2ad45d5
pgzmnk <pgzmnk@gmail.com>
2024-05-20 10:24:07 -0600

    Update isochrone UDF v2

--- public/Get_Isochrone/Get_Isochrone.py
+++ public/Get_Isochrone/Get_Isochrone.py
@@ -2,7 +2,7 @@
 def udf(
     lat=40.776732, lng=-73.873708, costing="auto", time_steps=[1, 5, 10, 15, 20, 25, 30]
 ):
-    # costing options: auto, pedestrian, bicycle,s truck, bus, motor_scooter
+    # costing options: auto, pedestrian, bicycle, truck, bus, motor_scooter
     from utils import get_isochrone
 
     gdf = get_isochrone(lat, lng, costing=costing, time_steps=time_steps)
