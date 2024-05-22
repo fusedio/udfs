9e622534ba10b27f42c41ca63d03f59bda0ac0d0
Yash Bharti <yashbharti@Yashs-MacBook-Pro.local>
2024-05-22 11:08:47 +0530

    Made Changes: Try/Catch for -> GEE Authentication, API Setup and Service Account Setup

--- community/yashbit/biogas_transportation/biogas_transportation.py
+++ community/yashbit/biogas_transportation/biogas_transportation.py
@@ -1,47 +1,49 @@
 @fused.udf
-def udf(bbox: fused.types.TileGDF=None, n=10):
+def udf(bbox: fused.types.TileGDF = None, n = 10):
     import pandas as pd
-    import ee 
-    import xarray 
-    import core_utils 
+    import ee
+    import xarray
+    import core_utils
     import json
     import geopandas as gpd
-    import folium 
-    import matplotlib.pyplot as plt 
-    import os 
-    import shapely 
+    import folium
+    import matplotlib.pyplot as plt
+    import os
+    import shapely
     from shapely.geometry import LineString
+    
     utils = fused.load(
         "https://github.com/fusedio/udfs/tree/f928ee1/public/common/"
-    ).utils 
-    """
-        User Needs to: Create GCP Account (Free) and do the following:
-        
-        Credentials:
-        1. Create Google Cloud Project
-        2. IAM -> Service Accounts
-        3. Grant Full Access
-        4. Copy Details
-        5. Register project for Google Earth Engine
-        6. Enable GEE API for Google Earth Engine
-        
-        @@@ NOTE: PLEASE ADD YOUR API KEYS in core_utils.py module
-
-        Visit the website:
-        https://developers.google.com/earth-engine/guides/access
-
-    """
-    service_account_info = core_utils.generate_service_account_info()
-    credentials = ee.ServiceAccountCredentials(
-        "", service_account_info
-    )
+    ).utils
     
+    """
+    User Needs to: Create GCP Account (Free) and do the following:
     
-    ee.Initialize(
-        opt_url="https://earthengine.googleapis.com", credentials=credentials
-    ) 
+    Credentials:
+    1. Create Google Cloud Project
+    2. IAM -> Service Accounts
+    3. Grant Full Access
+    4. Copy Details
+    5. Register project for Google Earth Engine
+    6. Enable GEE API for Google Earth Engine
+    
+    @@@ NOTE: PLEASE ADD YOUR API KEYS in core_utils.py module
+
+    Visit the website:
+    https://developers.google.com/earth-engine/guides/access
+    """
+    
+    try:
+        service_account_info = core_utils.generate_service_account_info()
+        credentials = ee.ServiceAccountCredentials("", service_account_info)
+        ee.Initialize(opt_url="https://earthengine.googleapis.com", credentials=credentials)
+    except Exception as e:
+        print("Error initializing Earth Engine. Please ensure that you have set up your API keys correctly in core_utils.py.")
+        print(f"Detailed error: {e}")
+        return None
+    
     # Plot Methane Hotspots
-    methane_air_l4 = ee.FeatureCollection("EDF/MethaneSAT/MethaneAIR/methaneair-L4point-2021")   
+    methane_air_l4 = ee.FeatureCollection("EDF/MethaneSAT/MethaneAIR/methaneair-L4point-2021")
     info = methane_air_l4.getInfo()
     features_2 = info['features']
     gdf = gpd.GeoDataFrame.from_features(features_2)
@@ -53,20 +55,21 @@ def udf(bbox: fused.types.TileGDF=None, n=10):
     for key, values in gdf_with_geojson.items():
         if key == 'geometry':
             features_2.extend(values)
-   
+
     gdf_from_features_2 = gpd.GeoDataFrame.from_features(features_2)
     
     # Let's Plot the Gas Pipelines
     file_path_gas_pipelines = 'https://docs.google.com/spreadsheets/d/12sd92oueYtnSqyJPCk1KY4RetzRS4L18/export?format=csv'
     gas_pipeline_df = pd.read_csv(file_path_gas_pipelines)
-    clean_df = gas_pipeline_df.drop(gas_pipeline_df[(gas_pipeline_df['WKTFormat']=='--') | (gas_pipeline_df['LengthEstimateKm']=='--') | (gas_pipeline_df['CapacityBcm/y']=='--')].index)
-    df_sum = clean_df.groupby('StartCountry').agg({'LengthEstimateKm':'sum','CapacityBcm/y':'sum'}).reset_index()
+    clean_df = gas_pipeline_df.drop(gas_pipeline_df[(gas_pipeline_df['WKTFormat'] == '--') | (gas_pipeline_df['LengthEstimateKm'] == '--') | (gas_pipeline_df['CapacityBcm/y'] == '--')].index)
+    df_sum = clean_df.groupby('StartCountry').agg({'LengthEstimateKm': 'sum', 'CapacityBcm/y': 'sum'}).reset_index()
     geo_df = gpd.GeoDataFrame(clean_df)
-    geo_df = geo_df.drop(geo_df[(geo_df['WKTFormat']=='--')].index)
+    geo_df = geo_df.drop(geo_df[(geo_df['WKTFormat'] == '--')].index)
     us_geo_df = geo_df[geo_df['StartCountry'] == 'USA']
-    linestring_df =us_geo_df[~us_geo_df.WKTFormat.str.contains('MULTILINESTRING')].copy()
+    linestring_df = us_geo_df[~us_geo_df.WKTFormat.str.contains('MULTILINESTRING')].copy()
     linestring_df['ls'] = linestring_df.WKTFormat.apply(core_utils.convert_linestr_to_linestr)
-    linestring_df.head()  
+    linestring_df.head()
+    
     features = []
     for feature, name, startcountry, pipe in zip(linestring_df.ls, linestring_df.Countries, linestring_df.StartCountry, linestring_df.PipelineName):
         if isinstance(feature, shapely.geometry.linestring.LineString):
@@ -76,8 +79,7 @@ def udf(bbox: fused.types.TileGDF=None, n=10):
         else:
             continue
         for linestring in linestrings:
-            
-            new_coordinates = [[x, y ] for x, y in linestring.coords]
+            new_coordinates = [[x, y] for x, y in linestring.coords]
             feature_dict = {
                 "type": "Feature",
                 "geometry": {
@@ -93,6 +95,7 @@ def udf(bbox: fused.types.TileGDF=None, n=10):
                 }
             }
             features.append(feature_dict)
+    
     geojson_data = {
         "type": "FeatureCollection",
         "features": features
@@ -102,9 +105,3 @@ def udf(bbox: fused.types.TileGDF=None, n=10):
     geojson_str = json.dumps(geojson_data)
     combined_gdf = core_utils.combine_gdfs(gdf_from_features, gdf_from_features_2)
     return combined_gdf
-    
-        
-      
-    
-
-
