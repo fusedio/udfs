78fdfa3f3c8a4a66630a3b600a5ffa84ccb87fac
Alex Martin <28570222+alexlowellmartin@users.noreply.github.com>
2024-06-12 15:57:36 -0500

    Created fun simple NWS Hazards UDF (#95)
    This UDF is intended to fetch current National Weather Service /
    National Oceanic and Atmospheric Administration hazard boundary
    polygons, fill them h3 cells, and return a gdf.

---
+++ public/NWS_Hazards_H3/NWS_Hazards_H3.py
@@ -0,0 +1,43 @@
+@fused.udf
+def udf(
+    bbox: fused.types.TileGDF=None,
+    crs="EPSG:4326", 
+    res=7
+):
+    
+    import pandas as pd
+    import geopandas as gpd
+    import h3
+    import requests
+    from utils import add_rgb_cmap, CMAP 
+
+    # URL for querying the WatchesWarnings layer of the Watch/Warning/Advisory (WWA) MapServer
+    url = "https://mapservices.weather.noaa.gov/eventdriven/rest/services/WWA/watch_warn_adv/MapServer/1/query"
+    
+    params = {
+    "where": "1=1",
+    "outFields": "*", 
+    "f": "geojson",
+    "geometryType": "esriGeometryEnvelope",
+    "geometry": bbox,
+    "inSR": "4326",
+    "spatialRel": "esriSpatialRelEnvelopeIntersects"
+    }
+    
+    # Fetch data
+    response = requests.get(url, params=params)
+    data = response.json()
+    
+    # Convert the GeoJSON data to a GeoDataFrame
+    gdf = gpd.GeoDataFrame.from_features(data["features"], crs=crs)
+
+    # Convert GeoDataFrame geos to h3 cells
+    cell_column = gdf.geometry.apply(lambda x: h3.geo_to_cells(x, res=res))
+    shape_column = cell_column.apply(h3.cells_to_h3shape)
+    gdf.geometry = shape_column
+
+
+    # Add 'r', 'g', and 'b' fields to the GeoDataFrame
+    gdf = add_rgb_cmap(gdf=gdf,key_field='prod_type', cmap_dict=CMAP)
+    
+    return gdf
