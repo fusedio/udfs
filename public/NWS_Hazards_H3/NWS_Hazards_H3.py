68e305059959be8322f4499ec6f2063386bae979
pgzmnk <pgzmnk@gmail.com>
2024-06-12 15:12:12 -0600

    Update NWS udf

--- public/NWS_Hazards_H3/NWS_Hazards_H3.py
+++ public/NWS_Hazards_H3/NWS_Hazards_H3.py
@@ -1,33 +1,32 @@
 @fused.udf
-def udf(
-    bbox: fused.types.TileGDF=None,
-    crs="EPSG:4326", 
-    res=7
-):
-    
-    import pandas as pd
+def udf(bbox: fused.types.TileGDF = None, crs="EPSG:4326", res=7):
+    import fused
     import geopandas as gpd
     import h3
     import requests
-    from utils import add_rgb_cmap, CMAP 
+    from utils import CMAP, add_rgb_cmap
 
     # URL for querying the WatchesWarnings layer of the Watch/Warning/Advisory (WWA) MapServer
     url = "https://mapservices.weather.noaa.gov/eventdriven/rest/services/WWA/watch_warn_adv/MapServer/1/query"
-    
+
     params = {
-    "where": "1=1",
-    "outFields": "*", 
-    "f": "geojson",
-    "geometryType": "esriGeometryEnvelope",
-    "geometry": bbox,
-    "inSR": "4326",
-    "spatialRel": "esriSpatialRelEnvelopeIntersects"
+        "where": "1=1",
+        "outFields": "*",
+        "f": "geojson",
+        "geometryType": "esriGeometryEnvelope",
+        "geometry": bbox,
+        "inSR": "4326",
+        "spatialRel": "esriSpatialRelEnvelopeIntersects",
     }
-    
+
     # Fetch data
-    response = requests.get(url, params=params)
+    @fused.cache
+    def fetch_data(params):
+        return requests.get(url, params=params)
+
+    response = fetch_data(params)
     data = response.json()
-    
+
     # Convert the GeoJSON data to a GeoDataFrame
     gdf = gpd.GeoDataFrame.from_features(data["features"], crs=crs)
 
@@ -36,8 +35,7 @@ def udf(
     shape_column = cell_column.apply(h3.cells_to_h3shape)
     gdf.geometry = shape_column
 
-
     # Add 'r', 'g', and 'b' fields to the GeoDataFrame
-    gdf = add_rgb_cmap(gdf=gdf,key_field='prod_type', cmap_dict=CMAP)
-    
+    gdf = add_rgb_cmap(gdf=gdf, key_field="prod_type", cmap_dict=CMAP)
+
     return gdf
