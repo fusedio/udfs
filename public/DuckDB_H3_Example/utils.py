da312a73a63defe70c2b6642cf55698de82b46b4
Isaac Brodsky <isaac@isaacbrodsky.com>
2024-06-17 08:28:10 -0700

    reformat

--- public/DuckDB_H3_Example/utils.py
+++ public/DuckDB_H3_Example/utils.py
@@ -1,5 +1,6 @@
 import fused
 
+
 def duckdb_with_h3(extra_config=None, extra_connect_args=None):
     import duckdb
 
@@ -17,10 +18,7 @@ def duckdb_with_h3(extra_config=None, extra_connect_args=None):
 def load_h3_duckdb(con):
     import duckdb
 
-    new_home_path = fused.core.create_path(
-        f"duckdb/{duckdb.__version__}/"
-    )
+    new_home_path = fused.core.create_path(f"duckdb/{duckdb.__version__}/")
     con.sql(f"SET home_directory='{new_home_path}';")
     con.sql("INSTALL h3ext from 'https://pub-cc26a6fd5d8240078bd0c2e0623393a5.r2.dev';")
     con.sql("LOAD h3ext;")
-
