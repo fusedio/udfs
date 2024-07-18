68792887abe45f9c0fe6d6dbc132af6530f8d603
Plinio Guzman <pgzmnk@gmail.com>
2024-07-18 15:20:15 -0600

    Update Raster-H3 UDF (#144)

--- community/pgzmnk/Raster_To_H3/Raster_To_H3.py
+++ community/pgzmnk/Raster_To_H3/Raster_To_H3.py
@@ -1,21 +1,19 @@
 @fused.udf
 def udf(
     tiff_path: str = "s3://fused-asset/gfc2020/JRC_GFC2020_V1_N10_E10.tif",
-    row_group_size=100_000,
-    cell_id: int = 1,
+    chunk_id: int = 1,
     x_chunks: int = 100,
     y_chunks: int = 100,
     h3_size=12,
 ):
-    import geopandas as gpd
     import pandas as pd
-    import pyarrow.parquet as pq
-    import rasterio
-    from shapely.geometry import box
-    from utils import chunked_tiff_to_points, get_chunk_slices_from_shape, run_query
 
-    df_tiff = chunked_tiff_to_points(
-        tiff_path, i=cell_id, x_chunks=x_chunks, y_chunks=y_chunks
+    utils = fused.load(
+        "https://github.com/fusedio/udfs/tree/43656f6/public/common/"
+    ).utils
+
+    df_tiff = utils.chunked_tiff_to_points(
+        tiff_path, i=chunk_id, x_chunks=x_chunks, y_chunks=y_chunks
     )
 
     qr = f"""
@@ -25,7 +23,7 @@ def udf(
         order by 1
     """
 
-    df = run_query(qr, return_arrow=True)
+    df = utils.run_query(qr, return_arrow=True)
     df = df.to_pandas()
     df["agg_data"] = df.agg_data.map(lambda x: pd.Series(x).sum())
     df["hex"] = df["hex"].map(lambda x: hex(x)[2:])
