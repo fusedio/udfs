4b0f73d47d992b483ce705325a6fc18acaa41569
pgzmnk <pgzmnk@gmail.com>
2024-07-30 15:15:43 -0600

    Update Greenest Pixel udf

--- public/Satellite_Greenest_Pixel/Satellite_Greenest_Pixel.py
+++ public/Satellite_Greenest_Pixel/Satellite_Greenest_Pixel.py
@@ -1,5 +1,7 @@
 import json
 
+import geopandas as gpd
+
 # https://planetarycomputer.microsoft.com/dataset/modis-09A1-061
 modis_params = json.dumps(
     {
@@ -42,67 +44,25 @@ sentinel_params = json.dumps(
 @fused.udf
 def udf(
     bbox: fused.types.TileGDF = None,
+    gdf_geom: gpd.GeoDataFrame = None,
     collection_params=sentinel_params,
     chip_len: int = 512,
     how: str = "max",  # median, min. default is max
     fillna: bool = False,  # This adresses stripes
 ):
-    import json
-
     import fused
     import numpy as np
     import numpy.ma as ma
     import pandas as pd
-    from utils import create_tiffs_catalog, run_pool_tiffs, search_pc_catalog
+    from utils import create_tiffs_catalog, run_pool_tiffs, run_udf, search_pc_catalog
 
-    collection, band_list, time_of_interest, query, scale = json.loads(
-        collection_params
-    ).values()
+    if gdf_geom is not None:
+        bbox = gdf_geom
 
-    print(collection, band_list, time_of_interest, query)
-
-    stac_items = search_pc_catalog(
-        bbox=bbox, time_of_interest=time_of_interest, query=query, collection=collection
+    return run_udf(
+        bbox=bbox,
+        collection_params=collection_params,
+        chip_len=chip_len,
+        how=how,
+        fillna=fillna,
     )
-    if not stac_items:
-        return
-
-    print("Processing stac_items: ", len(stac_items))
-    print(stac_items[0].assets.keys())
-    df_tiff_catalog = create_tiffs_catalog(stac_items, band_list)
-    arrs_out = run_pool_tiffs(bbox, df_tiff_catalog, chip_len)
-
-    # Calculate NDVI
-    # First 3 bands to visualize, last 2 bands to calculate NDVI
-    out = (arrs_out[-1] * 1.0 - arrs_out[-2] * 1.0) / (
-        arrs_out[-1] * 1.0 + arrs_out[-2] * 1.0
-    )
-    t_len = out.shape[0]
-    out_flat = out.reshape(t_len, chip_len * chip_len)
-    # Find greenest pixels
-    sorted_indices = np.argsort(out_flat, axis=0)
-    if how == "median":
-        median_index = sorted_indices[t_len // 2]
-    elif how == "min":
-        median_index = np.argmin(out_flat, axis=0)
-    else:
-        median_index = np.argmax(out_flat, axis=0)
-
-    out_flat = out_flat[median_index, np.arange(chip_len * chip_len)]
-
-    output_bands = []
-
-    for b in [0, 1, 2]:
-        out_flat = arrs_out[b].reshape(t_len, chip_len * chip_len)
-
-        # Replace 0s with NaNs
-        out_flat = np.where(out_flat == 0, np.nan, out_flat)
-        if fillna:
-            out_flat = pd.DataFrame(out_flat).ffill().bfill().values
-        out_flat = out_flat[median_index, np.arange(chip_len * chip_len)]
-        output_bands.append(out_flat.reshape(chip_len, chip_len))
-
-    stacked = np.stack(output_bands) * 1.0 * scale
-    stacked = np.clip(stacked, 0, 255).astype("uint8")
-    return stacked
-    # TODO: color correction https://custom-scripts.sentinel-hub.com/custom-scripts/sentinel-2/poor_mans_atcor/
