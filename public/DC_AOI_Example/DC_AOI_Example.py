133ce24f2847f736014c6f5007acf065fc38e2ec
kashuk <sina@nyu.edu>
2024-07-30 20:18:40 -0400

    add ndvi to dc_aoi

--- public/DC_AOI_Example/DC_AOI_Example.py
+++ public/DC_AOI_Example/DC_AOI_Example.py
@@ -1,8 +1,8 @@
 @fused.udf
-def udf(time_of_interest= "2021-09-01/2021-12-30",chip_len:int=None, scale:float=0.1):
+def udf(time_of_interest= "2021-09-01/2021-12-30",chip_len:int=None, scale:float=0.1,show_ndvi=True):
     import numpy as np   
     import geopandas as gpd 
-    from utils import get_arr
+    from utils import get_arr, rgbi_to_ndvi
     gdf = gpd.read_file('https://www2.census.gov/geo/tiger/TIGER_RD18/STATE/11_DISTRICT_OF_COLUMBIA/11/tl_rd22_11_tract.zip')
     # gdf = gdf.iloc[:1]
     xmin,ymin,xmax,ymax = fused.utils.common.geo_convert(gdf, crs='UTM').total_bounds
@@ -10,9 +10,20 @@ def udf(time_of_interest= "2021-09-01/2021-12-30",chip_len:int=None, scale:float
         chip_len = int(max(xmax-xmin,ymax-ymin)/10) #considering pixel size of 10m
     if chip_len>3000:
         raise ValueError(f'THe image os too big ({chip_len=}>3000). Consider reducing your area of interest.')  
-    print(chip_len)
-    arr = get_arr(gdf, time_of_interest, chip_len, nth_item=None)
-    arr = np.clip(arr * scale, 0, 255).astype("uint8")
+    print(f'{chip_len=}')
+    arr_rgbi = get_arr(gdf, time_of_interest, chip_len, nth_item=None)
+    if show_ndvi:
+        ndvi = rgbi_to_ndvi(arr_rgbi)
+        if isinstance(ndvi, np.ma.MaskedArray):
+            arr = fused.utils.common.arr_to_plasma(ndvi.data, colormap='RdYlGn', reverse=False)
+            arr = np.ma.masked_array(arr, [ndvi.mask]*3)
+        else:
+            arr = fused.utils.common.arr_to_plasma(ndvi, colormap='RdYlGn', reverse=False)
+    else:
+        arr = np.clip(arr_rgbi[:3] * scale, 0, 255).astype("uint8")
     geom_mask = fused.utils.common.gdf_to_mask_arr(gdf, arr.shape[-2:])
-    arr[:, geom_mask]=0
+    if len(arr.shape)==2:
+        arr = np.ma.masked_array(arr, [geom_mask])
+    else:
+        arr = np.ma.masked_array(arr, [geom_mask]*arr.shape[0])
     return arr, gdf.total_bounds
--- public/DC_AOI_Example/utils.py
+++ public/DC_AOI_Example/utils.py
@@ -2,18 +2,27 @@
 def get_arr(bbox, time_of_interest, chip_len, nth_item=None, max_items=30):
     import fused
     import numpy as np
-    utils = fused.load('https://github.com/fusedio/udfs/tree/7b97acb/public/Satellite_Greenest_Pixel').utils
+    utils = fused.load('https://github.com/fusedio/udfs/tree/bca4c9d/public/Satellite_Greenest_Pixel').utils
     stac_items = utils.search_pc_catalog(bbox=bbox, time_of_interest=time_of_interest, query={"eo:cloud_cover": {"lt": 5}}, collection="sentinel-2-l2a")
     if not stac_items:return
     df_tiff_catalog = utils.create_tiffs_catalog(stac_items, ["B02", "B03", "B04", "B08"])
     if len(df_tiff_catalog)>max_items:
         raise ValueError(f'{len(df_tiff_catalog)} > max number of images ({max_items})')  
-    arrs_out = utils.run_pool_tiffs(bbox, df_tiff_catalog, chip_len)
     if nth_item:
         if nth_item>len(df_tiff_catalog):
             raise ValueError(f'{nth_item} > total number of images ({len(df_tiff_catalog)})') 
         df_tiff_catalog = df_tiff_catalog[nth_item:nth_item+1]
-        arr=arrs_out[:3].squeeze()
+        arrs_out = utils.run_pool_tiffs(bbox, df_tiff_catalog, chip_len)
+        arr=arrs_out.squeeze()
     else:
+        arrs_out = utils.run_pool_tiffs(bbox, df_tiff_catalog, chip_len)
         arr = utils.get_greenest_pixel(arrs_out, how='median', fillna=True)
     return arr
+    
+@fused.cache
+def rgbi_to_ndvi(arr_rgbi):
+    import numpy as np
+    ndvi = (arr_rgbi[-1] * 1.0 - arr_rgbi[-2] * 1.0) / (
+        arr_rgbi[-1] * 1.0 + arr_rgbi[-2] * 1.0
+    )
+    return  (np.clip(ndvi,0,1)*255).astype('uint8')
