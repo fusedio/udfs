7b98f992ff77d588ccf31602e7e0138cac6a29be
kashuk <sina@nyu.edu>
2024-08-01 17:43:50 -0400

    update DC_AOI_Example

--- public/DC_AOI_Example/utils.py
+++ public/DC_AOI_Example/utils.py
@@ -1,8 +1,8 @@
 @fused.cache
-def get_arr(bbox, time_of_interest, chip_len, nth_item=None, max_items=30):
+def get_arr(bbox, time_of_interest, output_shape, nth_item=None, max_items=30):
     import fused
     import numpy as np
-    utils = fused.load('https://github.com/fusedio/udfs/tree/bca4c9d/public/Satellite_Greenest_Pixel').utils
+    utils = fused.load('https://github.com/fusedio/udfs/tree/9bfb5d0/public/Satellite_Greenest_Pixel').utils
     stac_items = utils.search_pc_catalog(bbox=bbox, time_of_interest=time_of_interest, query={"eo:cloud_cover": {"lt": 5}}, collection="sentinel-2-l2a")
     if not stac_items:return
     df_tiff_catalog = utils.create_tiffs_catalog(stac_items, ["B02", "B03", "B04", "B08"])
@@ -12,10 +12,10 @@ def get_arr(bbox, time_of_interest, chip_len, nth_item=None, max_items=30):
         if nth_item>len(df_tiff_catalog):
             raise ValueError(f'{nth_item} > total number of images ({len(df_tiff_catalog)})') 
         df_tiff_catalog = df_tiff_catalog[nth_item:nth_item+1]
-        arrs_out = utils.run_pool_tiffs(bbox, df_tiff_catalog, chip_len)
+        arrs_out = utils.run_pool_tiffs(bbox, df_tiff_catalog, output_shape)
         arr=arrs_out.squeeze()
     else:
-        arrs_out = utils.run_pool_tiffs(bbox, df_tiff_catalog, chip_len)
+        arrs_out = utils.run_pool_tiffs(bbox, df_tiff_catalog, output_shape)
         arr = utils.get_greenest_pixel(arrs_out, how='median', fillna=True)
     return arr
     
