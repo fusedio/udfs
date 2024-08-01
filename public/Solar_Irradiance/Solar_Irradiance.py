f02343fbba4120a15360dea55c764aaf975b18ee
pgzmnk <pgzmnk@gmail.com>
2024-07-31 18:11:11 -0600

    Solar udf

--- public/Solar_Irradiance/Solar_Irradiance.py
+++ public/Solar_Irradiance/Solar_Irradiance.py
@@ -1,28 +1,27 @@
 @fused.udf
 def udf(bbox: fused.types.Bbox = None):
     import geopandas as gpd
-    import shapely
+    from shapely import box
 
-    default_bbox = shapely.box(-122.549, 37.681, -122.341, 37.818)
-    tile_bbox_geom = bbox if bbox is not None else default_bbox
-    tile_bbox_gdf = gpd.GeoDataFrame({"geometry": [tile_bbox_geom]}, crs="EPSG:4326")
-    print(tile_bbox_geom)
+    tile_bbox_gdf = (
+        bbox
+        if bbox is not None
+        else gpd.GeoDataFrame(
+            {"geometry": [box(-122.549, 37.681, -122.341, 37.818)]}, crs="EPSG:4326"
+        )
+    )
 
     utils = fused.load(
         "https://github.com/fusedio/udfs/tree/3c4bc47/public/common/"
     ).utils
-    from utils import read_tiff
 
-    print(bbox)
-    print(tile_bbox_gdf)
-
-    input_tiff_path = f"s3://sunlightdatabucket/World_DNI_GISdata_LTAy_AvgDailyTotals_GlobalSolarAtlas-v2_GEOTIFF/DNI.tif"
-    data = read_tiff(
+    input_tiff_path = (
+        f"s3://fused-asset/solar_irradiance/DNI_LTAy_Avg_Daily_Totals_DNI.tif"
+    )
+    data = utils.read_tiff(
         bbox=tile_bbox_gdf, input_tiff_path=input_tiff_path, output_shape=(256, 256)
     )
-    print(data)
-    print(type(data))
 
     filled_data = data.filled(fill_value=0)
 
-    return utils.arr_to_plasma(filled_data, min_max=(0, 5), reverse=False)
+    return utils.arr_to_plasma(filled_data, min_max=(0, 6), reverse=False)
