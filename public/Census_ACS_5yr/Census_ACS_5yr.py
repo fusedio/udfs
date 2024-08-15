7e21fe91b4637d3ae244eb087d0dee6d261bdbd5
Isaac Brodsky <isaac@isaacbrodsky.com>
2024-08-15 16:59:03 -0700

    more pins (#210)

--- public/Census_ACS_5yr/Census_ACS_5yr.py
+++ public/Census_ACS_5yr/Census_ACS_5yr.py
@@ -1,33 +1,40 @@
 @fused.udf
-def udf(bbox, census_variable='Total Pop', scale_factor=200, is_density=True, year=2022):
+def udf(
+    bbox, census_variable="Total Pop", scale_factor=200, is_density=True, year=2022
+):
     from utils import acs_5yr_bbox
-    
-    #different geometry details per zoom level
-    if bbox.z[0]>12:
-        suffix=None
-    elif bbox.z[0]>10:
-        suffix='simplify_0001'
-    elif bbox.z[0]>8:
-        suffix='simplify_001'
-    elif bbox.z[0]>5:
-        suffix='simplify_01'
+
+    # different geometry details per zoom level
+    if bbox.z[0] > 12:
+        suffix = None
+    elif bbox.z[0] > 10:
+        suffix = "simplify_0001"
+    elif bbox.z[0] > 8:
+        suffix = "simplify_001"
+    elif bbox.z[0] > 5:
+        suffix = "simplify_01"
     else:
-        suffix='centroid'
+        suffix = "centroid"
     print(suffix)
 
-    #read the variables
-    gdf=acs_5yr_bbox(bbox.total_bounds, census_variable=census_variable, suffix=suffix, year=year)
-    if len(gdf)==0:
+    # read the variables
+    gdf = acs_5yr_bbox(
+        bbox.total_bounds, census_variable=census_variable, suffix=suffix, year=year
+    )
+    if len(gdf) == 0:
         return None
-    
-    #shorten the column name
-    gdf.columns = gdf.columns.map(lambda x:(str(x.split('|')[0])+str(x.split('|')[-1])) if '|' in x else x)
+
+    # shorten the column name
+    gdf.columns = gdf.columns.map(
+        lambda x: (str(x.split("|")[0]) + str(x.split("|")[-1])) if "|" in x else x
+    )
     print(gdf.columns)
 
-    #create a metric columns for the visualization
+    # create a metric columns for the visualization
     import numpy as np
-    if suffix=='centroid' or is_density==False:
-        gdf['metric']=gdf.iloc[:,2]*scale_factor/1000
+
+    if suffix == "centroid" or is_density == False:
+        gdf["metric"] = gdf.iloc[:, 2] * scale_factor / 1000
     else:
-        gdf['metric']=np.sqrt(gdf.iloc[:,2]/gdf.area)*scale_factor/1000
+        gdf["metric"] = np.sqrt(gdf.iloc[:, 2] / gdf.area) * scale_factor / 1000
     return gdf
