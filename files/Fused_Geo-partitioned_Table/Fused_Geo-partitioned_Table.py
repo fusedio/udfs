dfc2a2764eb328b92ba31a4b9dd0692be43054ed
plinio@fused.io <plinio@fused.io>
2024-08-31 03:52:22 +0000

    ththt

--- files/Fused_Geo-partitioned_Table/Fused_Geo-partitioned_Table.py
+++ files/Fused_Geo-partitioned_Table/Fused_Geo-partitioned_Table.py
@@ -1,9 +1,47 @@
 @fused.udf
-def udf(bbox: fused.types.TileGDF, path: str, preview: bool):
-    use_columns = ["geometry"] if preview else None
-    utils = fused.load(
-        "https://github.com/fusedio/udfs/tree/19b5240/public/common/"
-    ).utils
-    base_path = path.rsplit("/", maxsplit=1)[0] if path.endswith("/_sample") else path
-    df = utils.table_to_tile(bbox, table=base_path, use_columns=use_columns)
-    return df
+def udf(
+    bbox: fused.types.TileGDF, 
+    n_trips_threshold: int = 0
+):
+    import geopandas as gpd
+    import shapely
+    import duckdb
+    utils = fused.load("https://github.com/fusedio/udfs/tree/19b5240/public/common/").utils
+
+    # 1. Load weights
+    # FAF5 Total Truck Flows by Commodity_2022.csv
+    con = duckdb.connect()
+    @fused.cache
+    def load_weights():
+        return con.sql("""
+            SELECT 
+                ID::INTEGER AS ID, 
+                "AB Tons_22 All", 
+                "BA Tons_22 All", 
+                "AB Trips_22 All", 
+                "BA Trips_22 All", 
+                "TOT Tons_22 All", 
+                "TOT Trips_22 All" 
+            FROM read_csv('s3://fused-users/fused/plinio/charm/FAF5_Truck_Flows/FAF5 Total Truck Flows by Commodity_2022.csv') 
+            """).df()        
+    df_weight = load_weights()
+
+    # 2. Load links in the FAF network
+    path = 's3://fused-users/fused/plinio/charm/faf_links_parquet/'
+    use_columns = ['ID', 'LENGTH', 'DIR', 'DATA1', 'Class', 'Class_Description', 'Road_Name','Speed_Limit','geometry']
+    gdf = utils.table_to_tile(bbox, table=path, use_columns=use_columns, min_zoom=5)
+    df_weight['ID'] = df_weight['ID'].astype('int64')
+    df_weight['ID'] = df_weight['ID'].astype('int64')
+    gdf['ID'] = gdf['ID'].astype('int64')
+
+    # 3. Join
+    @fused.cache
+    def perform_join(bbox):
+        out = gdf.join(df_weight.set_index('ID'), on='ID', how='inner', lsuffix='_left', rsuffix='_right')
+        out.columns = out.columns.str.replace(' ', '')
+        return out
+    out = perform_join(bbox)
+    out = out[out['BATrips_22All'] >= n_trips_threshold]
+    print(out)
+    return out
+
