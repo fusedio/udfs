34e8ab49be6814e22e33a3e0b7f0fb149840d24a
sina <sina@fused.io>
2024-09-06 11:44:37 +0000

    add res factor

--- public/DEM_10m_Tile_Example/DEM_10m_Tile_Example.py
+++ public/DEM_10m_Tile_Example/DEM_10m_Tile_Example.py
@@ -1,5 +1,5 @@
 @fused.udf
-def udf(bbox, collection="3dep-seamless", band="data"):
+def udf(bbox, collection="3dep-seamless", band="data", res_factor:int=1):
     utils = fused.load(
         "https://github.com/fusedio/udfs/tree/f928ee1/public/common/"
     ).utils
@@ -12,13 +12,13 @@ def udf(bbox, collection="3dep-seamless", band="data"):
         "https://planetarycomputer.microsoft.com/api/stac/v1",
         modifier=planetary_computer.sign_inplace,
     )
-    items = catalog.search(
+    items = catalog.search( 
         collections=[collection],
         bbox=bbox.total_bounds,
     ).item_collection()
-    print(items[0].assets.keys())
+    print(items[0].assets.keys()) 
     print(f"Returned {len(items)} Items")
-    resolution = int(20 * 2 ** (max(0, 13 - bbox.z[0])))
+    resolution = int(20/res_factor * 2 ** (max(0, 13 - bbox.z[0])))
     print(f"{resolution=}")
     ds = odc.stac.load(
         items,
