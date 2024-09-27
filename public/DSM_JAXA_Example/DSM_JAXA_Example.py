a3ea922aa6ed691c401612b4d59a41fc8c2cd2f9
Tyler Erickson <tyler@vorgeo.com>
2024-09-27 15:29:35 -0700

    Update DSM_JAXA example to use .visualize()

--- public/DSM_JAXA_Example/DSM_JAXA_Example.py
+++ public/DSM_JAXA_Example/DSM_JAXA_Example.py
@@ -1,7 +1,21 @@
-# ref: https://data.earth.jaxa.jp/app/viewer/v1/?collection=https://s3.ap-northeast-1.wasabisys.com/je-pds/cog/v1/JAXA.EORC_ALOS.PRISM_AW3D30.v3.2_global/collection.json&band=DSM&date=2021-02-01T00:00:00.000Z
 @fused.udf
-def udf(bbox, min_max=(0, 255), z_levels=[4, 6, 9, 11], verbose=False):
+def udf(
+    bbox,
+    min_max=(0, 255),
+    z_levels=[4, 6, 9, 11],
+    verbose=False
+):
+    """Display a digital surface model from JAXA
+    
+    ref: https://data.earth.jaxa.jp/app/viewer/v1/?collection=https://s3.ap-northeast-1.wasabisys.com/je-pds/cog/v1/JAXA.EORC_ALOS.PRISM_AW3D30.v3.2_global/collection.json&band=DSM&date=2021-02-01T00:00:00.000Z
+    """
+    import palettable
     import utils
-
+    
     arr = utils.dsm_to_tile(bbox, z_levels=z_levels, verbose=verbose)
-    return utils.arr_to_plasma(arr, min_max=min_max)
+    return utils.visualize(
+        data=arr,
+        min=0,
+        max=3000,
+        colormap=palettable.matplotlib.Viridis_20,
+    )
