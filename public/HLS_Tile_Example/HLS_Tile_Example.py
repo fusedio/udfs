9fbe370cb2468e377ca1842957500efc9c40e2eb
tyler@vorgeo.com <tyler@vorgeo.com>
2024-10-09 21:12:01 +0000

    Fix broken collection & clean up

--- public/HLS_Tile_Example/HLS_Tile_Example.py
+++ public/HLS_Tile_Example/HLS_Tile_Example.py
@@ -1,27 +1,44 @@
-# To Get your username and password, Please visit https://urs.earthdata.nasa.gov
 @fused.udf
 def udf(
     bbox,
-    collection_id="HLSS30.v2.0",  # Landsat:'HLSL30.v2.0' & Sentinel:'HLSS30.v2.0'
+    collection_id="HLSS30_2.0",  # Landsat:'HLSL30_2.0' & Sentinel:'HLSS30_2.0'
     band="B8A",  # Landsat:'B05' & Sentinel:'B8A'
-    date_range="2023-11/2024-01",
+    date_range="2023-10/2024-01",
     max_cloud_cover=25,
     n_mosaic=5,
     min_max=(0, 3000),
-    username="your_username",
-    password="your_password",
+    username="username",
+    password="password",
     env="earthdata",
 ):
+    
+    import numpy as np
+    import palettable
+    from pystac_client import Client
+    import utils
+    
+    STAC_URL = "https://cmr.earthdata.nasa.gov/stac"
+
     z = bbox.z[0]
     if z >= 9:
-        utils = fused.load(
-            "https://github.com/fusedio/udfs/tree/f928ee1/public/common/"
-        ).utils
-        import numpy as np
-        from pystac_client import Client
 
-        STAC_URL = "https://cmr.earthdata.nasa.gov/stac"
         catalog = Client.open(f"{STAC_URL}/LPCLOUD/")
+    
+        # Uncomment the following line to see a list of collections.
+        # utils.list_stac_collections(catalog)
+
+        # Check authentication.
+        cred = {"env": env, "username": username, "password": password}
+        try:
+            aws_session = utils.earth_session(cred=cred)
+        except:
+            print("Unable to authenticate to NASA Earthdata.\n"
+                  "Please update the `username` and `password` arguments.\n"
+                  "To register and manage your Earthdata Login account,\n"
+                  "go to: https://urs.earthdata.nasa.gov "
+            )
+            return None
+
         search = catalog.search(
             collections=[collection_id],
             bbox=bbox.total_bounds,
@@ -42,35 +59,31 @@ def udf(
                 )
                 band_urls.append(url)
         if len(band_urls) == 0:
-            print(
-                "No items were found. Please check your `collection_id` and `band` and widen your `date_range`."
-            )
+            try:
+                collection = catalog.get_collection(collection_id)
+                print(f"Collection '{collection_id}' exists")
+                print(
+                    "No items were found. Please check your `band` and/or widen your `date_range`."
+                )
+            except Exception:
+                print(f"Collection '{collection_id}' was not found in the catalog.")
             return None
-        try:
-            aws_session = utils.earth_session(
-                cred={"env": env, "username": username, "password": password}
-            )
-        except:
-            print(
-                "Please set `username` and `password` arguments. Create credentials at: https://urs.earthdata.nasa.gov to register and manage your Earthdata Login account."
-            )
-            return None
-        cred = {"env": env, "username": username, "password": password}
-        mosaic_reduce_function = lambda x: np.max(x, axis=0)
+
         arr = utils.mosaic_tiff(
             bbox,
             band_urls[:n_mosaic],
-            mosaic_reduce_function,
+            reduce_function=lambda x: np.max(x, axis=0),
             overview_level=max(0, 12 - z),
             cred=cred,
         )
-        output = utils.arr_to_plasma(arr, min_max)
-        print(f"{i.assets.keys()=}")
-        print(f"{len(band_urls)=}")
-        print(f"{arr.min()=}")
-        print(f"{arr.max()=}")
-
-        return output
+        # Visualize data as an RGB image.
+        rgb_image = utils.visualize(
+            data=arr,
+            min=min_max[0],
+            max=min_max[1],
+            colormap=palettable.scientific.sequential.Oslo_20,
+        )
+        return rgb_image
     elif z >= 8:
         print("Almost there! Please zoom in a bit more. 😀")
     else:
