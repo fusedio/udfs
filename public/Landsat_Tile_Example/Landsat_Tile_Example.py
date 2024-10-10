ab06ad5c838a9875346526c1f927618b6b031a3b
Tyler Erickson <tyler@vorgeo.com>
2024-10-10 16:15:27 -0700

    Clean up Landsat Tile example

--- public/Landsat_Tile_Example/Landsat_Tile_Example.py
+++ public/Landsat_Tile_Example/Landsat_Tile_Example.py
@@ -5,36 +5,53 @@ def udf(
     red_band="red",
     nir_band="nir08",
     collection="landsat-c2-l2",
+    cloud_threshold=10,
 ):
-    utils = fused.load(
-        "https://github.com/fusedio/udfs/tree/f928ee1/public/common/"
-    ).utils
+    """Display NDVI based on Landsat & STAC"""
     import odc.stac
+    import palettable
     import pystac_client
     from pystac.extensions.eo import EOExtension as eo
 
-    odc.stac.configure_s3_access(requester_pays=True)
+    # Load utility functions.
+    utils = fused.load(
+        "https://github.com/fusedio/udfs/tree/5cfb808/public/common/"
+    ).utils
+
     catalog = pystac_client.Client.open("https://earth-search.aws.element84.com/v1")
+    
+    # Search for imagery within a specified bounding box and time period
     items = catalog.search(
         collections=[collection],
         bbox=bbox.total_bounds,
         datetime=time_of_interest,
-        query={"eo:cloud_cover": {"lt": 10}},
+        query={"eo:cloud_cover": {"lt": cloud_threshold}},
     ).item_collection()
-    # least_cloudy_item = min(items, key=lambda item: eo.ext(item).cloud_cover)
-    # print(least_cloudy_item.assets.keys())
     print(f"Returned {len(items)} Items")
-    resolution = int(5 * 2 ** (15 - bbox.z[0]))
-    print(f"{resolution=}")
+
+    # Determine the pixel spacing for the zoom level
+    pixel_spacing = int(5 * 2 ** (15 - bbox.z[0]))
+    print(f"{pixel_spacing = }")
+
+    # Load imagery into an XArray dataset
+    # odc.stac.configure_s3_access(requester_pays=True)
     ds = odc.stac.load(
         items,
         crs="EPSG:3857",
         bands=[nir_band, red_band],
-        resolution=resolution,
+        resolution=pixel_spacing,
         bbox=bbox.total_bounds,
     ).astype(float)
+
+    # Calculate the Normalized Difference Vegetation Index
     ndvi = (ds[nir_band] - ds[red_band]) / (ds[nir_band] + ds[red_band])
-    print(ndvi.shape)
+
+    # Select the maximum value across all times
     arr = ndvi.max(dim="time")
-    # arr = ndvi.groupby("time.month").median()[0]
-    return utils.arr_to_plasma(arr.values, min_max=(0, 0.5))
+  
+    return utils.visualize(
+        arr.values,
+        min=0,
+        max=0.5,
+        colormap=palettable.scientific.sequential.Bamako_20_r,
+    )
