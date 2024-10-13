74990b91940a19bde034d3486d2acd1a9666e53c
sina@fused.io <sina@fused.io>
2024-10-13 06:21:33 +0000

    add sqrt metric

--- public/NYC_TLC_Hourly_2010/NYC_TLC_Hourly_2010.py
+++ public/NYC_TLC_Hourly_2010/NYC_TLC_Hourly_2010.py
@@ -1,10 +1,11 @@
 @fused.udf
-def udf(path: str='s3://fused-asset/misc/nyc/TLC_2010_count_hourly_hex12.parquet'):
-    hex_res=11 #max value is 12
+def udf(hex_res:int=11, path: str='s3://fused-asset/misc/nyc/TLC_2010_count_hourly_hex12.parquet'):
     import h3
     import pandas as pd
+    hex_res=min(hex_res,12)
     @fused.cache
     def get_data(path, hex_res, hourly=False):
+        version='1.0'
         df = pd.read_parquet(path)   
         df['hex'] = df['hex'].map(lambda x:h3.cell_to_parent(x, hex_res))
         if hourly:
@@ -13,6 +14,7 @@ def udf(path: str='s3://fused-asset/misc/nyc/TLC_2010_count_hourly_hex12.parquet
             df = df.groupby('hex')['cnt'].sum().reset_index()
         return df
     df=get_data(path, hex_res, hourly=False)
-    print(df)  
-    df.cnt=df.cnt/df.cnt.max()*3000    
+    # df=df[df.cnt>=100]
+    df['metric']=df.cnt**0.5*10
+    print(df)    
     return df
