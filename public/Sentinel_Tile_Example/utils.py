7546becf676aa628a6e9302446a133d8b15337a7
tyler@vorgeo.com <tyler@vorgeo.com>
2024-10-16 22:55:47 +0000

    Clean up Sentinel_Tile example

---
+++ public/Sentinel_Tile_Example/utils.py
@@ -0,0 +1,7 @@
+arr_to_plasma = fused.load(
+    "https://github.com/fusedio/udfs/tree/2b25cb3/public/common/"
+).utils.arr_to_plasma
+
+visualize = fused.load(
+    "https://github.com/fusedio/udfs/tree/2b25cb3/public/common/"
+).utils.visualize
