434008efb42180dfa4c49028d478a16ef6f8e57b
carmen.mitru@gmail.com <carmen.mitru@gmail.com>
2024-11-18 17:59:24 +0000

    test1312

--- public/Solar_Irradiance/Solar_Irradiance.py
+++ public/Solar_Irradiance/Solar_Irradiance.py
@@ -1,31 +1,27 @@
-import geopandas as gpd
-from shapely import box
-
 @fused.udf
-def udf(
-    bbox: fused.types.TileGDF = gpd.GeoDataFrame(
-            {"geometry": [box(-122.549, 37.681, -122.341, 37.818)]},
-            crs="EPSG:4326")
-):
-    import palettable    
-    import utils
+def udf(bbox: fused.types.TileGDF = None):
+    import geopandas as gpd
+    from shapely import box
+
+    tile_bbox_gdf = (
+        bbox
+        if bbox is not None
+        else gpd.GeoDataFrame(
+            {"geometry": [box(-122.549, 37.681, -122.341, 37.818)]}, crs="EPSG:4326"
+        )
+    )
+
+    utils = fused.load(
+        "https://github.com/fusedio/udfs/tree/3c4bc47/public/common/"
+    ).utils
 
     input_tiff_path = (
         f"s3://fused-asset/solar_irradiance/DNI_LTAy_Avg_Daily_Totals_DNI.tif"
     )
     data = utils.read_tiff(
-        bbox=bbox,
-        input_tiff_path=input_tiff_path,
-        output_shape=(256, 256)
+        bbox=tile_bbox_gdf, input_tiff_path=input_tiff_path, output_shape=(256, 256)
     )
 
-    data = data.filled(fill_value=0)
-    
-    rgb_image = utils.visualize(
-        data=data,
-        min=0,
-        max=8,
-        colormap=palettable.scientific.sequential.LaJolla_20_r,
-    )
+    filled_data = data.filled(fill_value=0)
 
-    return rgb_image
+    return utils.arr_to_plasma(filled_data, min_max=(0, 6), reverse=False)
