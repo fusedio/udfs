f274f386f4ce9dc547e00fa123aac42da633c282
Isaac Brodsky <isaac@isaacbrodsky.com>
2024-11-20 12:16:47 -0800

    foursquare open source places (#444)

---
+++ public/Foursquare_Open_Source_Places/Foursquare_Open_Source_Places.py
@@ -0,0 +1,21 @@
+@fused.udf
+def udf(
+    bbox: fused.types.TileGDF,
+    release: str = "2024-11-19",
+    min_zoom: int = 6,
+    use_columns: list = ["geometry", "name", "fsq_category_ids"],
+):
+    from utils import join_fsq_categories
+
+    utils = fused.load(
+        "https://github.com/fusedio/udfs/tree/19b5240/public/common/"
+    ).utils
+
+    path = f"s3://us-west-2.opendata.source.coop/fused/fsq-os-places/{release}/places/"
+    df = utils.table_to_tile(
+        bbox, table=path, min_zoom=min_zoom, use_columns=use_columns
+    )
+
+    df = join_fsq_categories(df, release=release)
+
+    return df
