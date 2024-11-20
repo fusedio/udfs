8d8115b6067ea4b40e872e919947787df80dad76
Isaac Brodsky <isaac@isaacbrodsky.com>
2024-11-20 12:16:24 -0800

    foursquare open source places

---
+++ public/Foursquare_Open_Source_Places/utils.py
@@ -0,0 +1,40 @@
+def join_fsq_categories(df, *, release):
+    if "fsq_category_ids" in df.columns:
+        df = df.explode("fsq_category_ids")
+        df_cat = get_fsq_categories(release=release)
+        # 4/5/6 exist for some POIs
+        df = (
+            df.set_index("fsq_category_ids")
+            .join(
+                df_cat.set_index("category_id")[
+                    [
+                        "level1_category_name",
+                        "level2_category_name",
+                        "level3_category_name",
+                    ]
+                ]
+            )
+            .reset_index()
+        )
+
+    if "fsq_category_labels" in df.columns:
+        df["fsq_category_labels"] = df["fsq_category_labels"].apply(category_fixup)
+
+    return df
+
+
+@fused.cache
+def get_fsq_categories(release):
+    import pandas as pd
+
+    url = f"s3://fsq-os-places-us-east-1/release/dt={release}/categories/parquet/categories.snappy.parquet"
+    df_cat = pd.read_parquet(url)
+    return df_cat
+
+
+def category_fixup(x):
+    import numpy as np
+
+    if x is None:
+        return np.array(["NA"])
+    return x
