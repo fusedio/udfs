8fa6854ebcb7d5fb84a3715b7c297e9dc53f298c
Stephen <skent89@gmail.com>
2024-12-05 17:38:37 -0500

    commit "Coffee Buildings UDF"

---
+++ public/Buildings_Proximity_to_Coffee_Shops_Bushwick/Buildings_Proximity_to_Coffee_Shops_Bushwick.py
@@ -0,0 +1,39 @@
+@fused.udf
+def udf(bbox: fused.types.TileGDF=None, 
+        resolution: int=11,
+        costing="pedestrian", 
+        time_steps=[5] # 5 minutes
+       ):
+    
+    import shapely
+    import geopandas as gpd
+    import pandas as pd
+    from utils import get_iso_gdf, get_cells, bushwick_boundary
+    
+    # This pulls 5 minute walking isochrones around FSQ coffee shops
+    gdf_iso = get_iso_gdf(costing=costing, time_steps=time_steps)
+    
+    # Converts geometries to WKT for DuckDB 
+    gdf_iso['geometry'] = gdf_iso['geometry'].apply(shapely.wkt.dumps)
+
+    # Better to use Pandas with DuckDB
+    df_iso = pd.DataFrame(gdf_iso)
+    if df_iso is None or df_iso.empty:
+        return  
+    
+    # Convert the isochrone Polygons to H3
+    gdf_h3 = get_cells(df_iso, resolution)
+
+    # Overlay Bushwick Boundary
+    gdf_h3 = bushwick_boundary(gdf_h3)
+    
+    # Get Overture Buildings
+    gdf_overture = fused.utils.Overture_Maps_Example.get_overture(bbox=bbox, min_zoom=10)
+    
+    # Join H3 with Buildings using coffe_score to visualize, you can change to a left join
+    gdf_joined = gdf_overture.sjoin(gdf_h3, how="inner", predicate="intersects")
+
+    # See the cells 
+    # return gdf_h3
+    
+    return gdf_joined
