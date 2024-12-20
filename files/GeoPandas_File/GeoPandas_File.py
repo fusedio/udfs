b72ce21c40fec96b53acc3e6a2311e9d3a90c0cd
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-12-20 17:46:15 +0100

    use fsspec for GeoPandas_File UDF to support GCS (workaround pyogrio bug)

--- files/GeoPandas_File/GeoPandas_File.py
+++ files/GeoPandas_File/GeoPandas_File.py
@@ -1,6 +1,7 @@
 @fused.udf
 def udf(path: str, preview: bool):
     import geopandas as gpd
+    import fsspec
 
     extension = path.rsplit(".", maxsplit=1)[-1]
     driver = (
@@ -8,7 +9,9 @@ def udf(path: str, preview: bool):
         if extension == "gpkg"
         else ("ESRI Shapefile" if extension == "shp" else "GeoJSON")
     )
-    gdf = gpd.read_file(path, driver=driver)
+
+    with fsspec.open(path) as f:
+        gdf = gpd.read_file(path, driver=driver)
     print(gdf)
     if preview:
         return gdf.geometry
