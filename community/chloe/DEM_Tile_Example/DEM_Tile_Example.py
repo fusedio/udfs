505078a5ccd71a9c4e6218cfe27e9a7a40664162
chloe@fused.io <chloe@fused.io>
2024-12-23 18:38:40 +0000

    test

---
+++ community/chloe/DEM_Tile_Example/DEM_Tile_Example.py
@@ -0,0 +1,60 @@
+def udf(
+    bbox: fused.types.TileGDF = None,
+    provider: str = "AWS"
+):
+    import odc.stac
+    import palettable
+    import planetary_computer
+    import pystac_client
+    from pystac.extensions.eo import EOExtension as eo
+    import utils
+
+    print(f'{type(bbox) = }')
+    
+    collection = "cop-dem-glo-30"
+
+    if provider == "AWS":
+        odc.stac.configure_s3_access(aws_unsigned=True)
+        catalog = pystac_client.Client.open(
+            "https://earth-search.aws.element84.com/v1"
+        )
+    elif provider == "MSPC":
+        catalog = pystac_client.Client.open(
+            "https://planetarycomputer.microsoft.com/api/stac/v1",
+            modifier=planetary_computer.sign_inplace,
+        )
+    else:
+        raise ValueError(
+            f'{provider=} does not exist. provider options are "AWS" and "MSPC"'
+        )
+    
+    items = catalog.search(
+        collections=[collection],
+        bbox=bbox.total_bounds,
+    ).item_collection()
+    print(f"Returned {len(items)} Items")
+
+    # Calculate the resolution based on zoom level.
+    resolution = int(20 * 2 ** (13 - bbox.z[0]))
+    print(f"{resolution=}")
+
+    # Load the data into an XArray dataset
+    xarray_dataset = odc.stac.load(
+        items,
+        crs="EPSG:3857",
+        bands=["data"],
+        resolution=resolution,
+        bbox=bbox.total_bounds,
+    ).astype(float)
+    
+    # Use data from the most recent time.
+    arr = xarray_dataset["data"].max(dim="time")
+
+    # Visualize that data as an RGB image.
+    rgb_image = utils.visualize(
+        data=arr,
+        min=0,
+        max=3000,
+        colormap=palettable.matplotlib.Viridis_20,
+    )
+    return rgb_image
