384b134318fe6c2985776bda7fffb16a529e74c0
kristinscholten@gmail.com <kristinscholten@gmail.com>
2025-01-02 13:59:14 +0000

    making this udf public

---
+++ public/Solar_Induced_Fluorescence/Solar_Induced_Fluorescence.py
@@ -0,0 +1,38 @@
+@fused.udf
+def udf(bbox, year: str = "2016", month: str = "05", period: str ="b", chip_len: int = 1024):
+    xy_cols = ['lon', 'lat']
+    from utils import get_masked_array, get_da, get_da_bounds, clip_arr
+    import pandas as pd
+    
+    # Dynamically construct the path based on the year and month
+    path = f's3://soldatanasasifglobalifoco2modis1863/Global_SIF_OCO2_MODIS_1863/data/sif_ann_{year}{month}{period}.nc'
+    
+    # Get the data array using the constructed path
+    da = get_da(path, coarsen_factor=1, variable_index=0, xy_cols=xy_cols)
+    
+    # Clip the array based on the bounding box
+    arr_aoi = clip_arr(da.values, 
+                       bounds_aoi=bbox.total_bounds, 
+                       bounds_total=get_da_bounds(da, xy_cols=xy_cols))
+    # Extract raw SIF values
+    sif_values = arr_aoi[arr_aoi != 0]  # Filter out zero values (no data areas)
+    
+    # Calculate statistics for raw SIF values
+    sif = sif_values.mean()
+    sum_sif = sif_values.sum()
+    count_sif = len(sif_values)
+
+    # Store in DataFrame
+    df_sif = pd.DataFrame([{'sum_sif': sum_sif, 'count_sif': count_sif, 'sif': sif}])
+    print(df_sif)
+
+    # Convert the array to an image with the specified colormap
+    img = (arr_aoi * 255).astype('uint8')
+    img = fused.utils.common.arr_to_plasma(arr_aoi, min_max=(0, 1), colormap="rainbow", include_opacity=False, reverse=False)
+
+    return img
+
+
+
+
+    
