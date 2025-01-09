b06265431fd0fdab25118d49ff702ef37aedc8a0
plinio@fused.io <plinio@fused.io>
2025-01-09 16:29:56 +0000

    Create Overture Buildings GERS lookup UDF

---
+++ community/plinio/Overture_Buildings_GERS_Lookup/Overture_Buildings_GERS_Lookup.py
@@ -0,0 +1,48 @@
+@fused.udf
+def udf(gers_id: str='08b2a100d2cb6fff02000821de8bdff1'):
+    import h3
+    from shapely.geometry import Polygon
+    import geopandas as gpd
+    import pandas as pd
+
+    # 1. H3 from GERS
+    h3_index = gers_id[:16]
+    print('h3_index', h3_index)
+
+    # 2. Polygon from H3
+    bounds = Polygon([coord[::-1] for coord in h3.cell_to_boundary(h3_index)])
+    bbox = gpd.GeoDataFrame({'h3_index': [h3_index], 'geometry': [bounds]})
+
+    # 3. Load Overture Buildings
+    gdf = fused.utils.Overture_Maps_Example.get_overture(bbox=bbox, overture_type='building', min_zoom=10)
+
+    # 4. Subselect building
+    gdf = gdf[gdf['id'] == gers_id]
+    
+    # 5. De-struct the names column
+    normalized_df = pd.json_normalize(gdf['names'])
+    gdf = gdf.reset_index(drop=True).join(normalized_df)
+    cols = ['id', 'primary', 'subtype', 'class', 'geometry']
+
+    print(gdf[cols].T)
+    return gdf[cols]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
