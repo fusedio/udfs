be74465713d475a4327c245cd3337227005177b3
plinio@fused.io <plinio@fused.io>
2025-01-10 22:52:26 +0000

    Introduce Overture_Buildings_GERS_Lookup

---
+++ community/plinio/Overture_Buildings_GERS_Enrich/Overture_Buildings_GERS_Enrich.py
@@ -0,0 +1,43 @@
+import json
+
+aoi = json.dumps({"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"coordinates":[[[-73.9945443706057,40.751205998161026],[-73.9945443706057,40.749882643934455],[-73.99222917395902,40.749882643934455],[-73.99222917395902,40.751205998161026],[-73.9945443706057,40.751205998161026]]],"type":"Polygon"}}]})
+
+@fused.udf
+def udf(bbox: fused.types.TileGDF=aoi):
+    import geopandas as gpd
+
+    # 1. Convert bbox to GeoDataFrame
+    if isinstance(bbox, str):
+        bbox = gpd.GeoDataFrame.from_features(json.loads(bbox))
+
+    # 2. Load Overture Buildings that intersect the given bbox centroid
+    gdf = fused.utils.Overture_Maps_Example.get_overture(bbox=bbox.geometry.centroid, overture_type='building', min_zoom=10)
+
+    # How many Overture buildings fall within the bbox centroid?
+    print("Buildings in centroid: ", len(gdf))
+    
+    # 3. Rule to set only one GERS on the input polygon
+    bbox['id'] = gdf.id.values[0]
+    print(bbox['id'])
+    
+    return bbox
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
