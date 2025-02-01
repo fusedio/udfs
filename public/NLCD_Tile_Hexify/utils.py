79567b6bc35adb11bfa12b637779d1f83972f5b3
sina@fused.io <sina@fused.io>
2025-02-01 17:46:36 -0500

    update to use latest NLCD

--- public/NLCD_Tile_Hexify/utils.py
+++ public/NLCD_Tile_Hexify/utils.py
@@ -1,4 +1,4 @@
-arr_to_h3 = fused.load("https://github.com/fusedio/udfs/tree/7204a3c/public/common/").utils.arr_to_h3
-get_data = fused.load('https://github.com/fusedio/udfs/tree/7204a3c/public/NLCD_Tile_Example/').utils.get_data
-rgb_to_hex = fused.load('https://github.com/fusedio/udfs/tree/7204a3c/public/NLCD_Tile_Example/').utils.rgb_to_hex
-nlcd_category_dict = fused.load('https://github.com/fusedio/udfs/tree/7204a3c/public/NLCD_Tile_Example/').utils.nlcd_category_dict
+arr_to_h3 = fused.load("https://github.com/fusedio/udfs/tree/27999aa/public/common/").utils.arr_to_h3
+get_data = fused.load('https://github.com/fusedio/udfs/tree/27999aa/public/NLCD_Tile_Example/').utils.get_data
+rgb_to_hex = fused.load('https://github.com/fusedio/udfs/tree/27999aa/public/NLCD_Tile_Example/').utils.rgb_to_hex
+nlcd_category_dict = fused.load('https://github.com/fusedio/udfs/tree/27999aa/public/NLCD_Tile_Example/').utils.nlcd_category_dict
