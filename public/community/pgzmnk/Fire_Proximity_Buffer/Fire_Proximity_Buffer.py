36332a2e501f1f4d9eacceb8bfed9141a6f7d188
Suryashankar Das <suryashankardas.2002@gmail.com>
2025-03-06 14:51:50 +0530

    delete duplicate UDFs in incorrect folders [1/2]

--- public/community/pgzmnk/Fire_Proximity_Buffer/Fire_Proximity_Buffer.py
+++
@@ -1,9 +0,0 @@
-@fused.udf
-def udf(
-    bbox: fused.types.TileGDF=None,
-    date_start: int = 2,
-):
-
-    from utils import load_fire_buffer_gdf
-    gdf_fire = load_fire_buffer_gdf()
-    return gdf_fire
