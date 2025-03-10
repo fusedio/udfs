0efa17f5b0940e18f53583fae77a7ed868416086
Sina Kashuk <sina@nyu.edu>
2025-03-10 08:44:47 -0400

    add dict support for xyz (#705)
    add dict support for xyz
    
    Made in [Fused Workbench](https://www.fused.io/workbench)

--- public/common/utils.py
+++ public/common/utils.py
@@ -1201,6 +1201,13 @@ def geo_convert(
     import pandas as pd
     import mercantile
     
+    # Convert xyz dict to xyz array
+    if isinstance(data, dict) and set(data.keys()) == {'x', 'y', 'z'}:
+        try:
+            data = [int(data['x']), int(data['y']), int(data['z'])]
+        except (ValueError, TypeError):
+            pass     
+            
     # Handle the bounds case specifically
     if data is None or (isinstance(data, (list, tuple, np.ndarray)) and len(data) == 4):
         bounds = [-180, -90, 180, 90] if data is None else data
