ed2b1d674b8adaa6443d0a40e55e22e63af28399
Suryashankar Das <suryashankardas.2002@gmail.com>
2025-03-25 17:05:01 +0530

    add tests for files udfs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,81 @@
 import os
+import pytest
+import tempfile
 
 import fused
+import geopandas as gpd
+from shapely.geometry import Point, LineString
+import pandas as pd
+import numpy as np
+import xarray as xr
 
 # Authenticate
 if os.environ.get("CI"):
     fused._auth.refresh_token(os.getenv("AUTH0_REFRESH_TOKEN"))
+
+
+@pytest.fixture
+def temp_directory():
+    """Create a temporary directory for test files."""
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        yield tmp_dir
+
+
+@pytest.fixture
+def sample_dataframe():
+    """Helper function to create a sample pandas DataFrame"""
+    return pd.DataFrame(
+        {
+            "id": [1, 2, 3],
+            "name": ["Alice", "Bob", "Charlie"],
+            "value": [10.1, 20.2, 30.3],
+        }
+    )
+
+
+@pytest.fixture
+def sample_geodataframe():
+    """Helper function to create a sample GeoPandas GeoDataFrame"""
+    geometry = [
+        Point(x, y) for x, y in zip([-122.4, -122.5, -122.6], [37.8, 37.7, 37.6])
+    ]
+    return gpd.GeoDataFrame(
+        pd.DataFrame(
+            {
+                "data": pd.Series([1, 2, 3], dtype="int32"),
+                # geopandas converts column to int32 after reading from file
+                # test will fail if column is not int32
+            }
+        ),
+        geometry=geometry,
+        crs="EPSG:4326",
+    )
+
+
+@pytest.fixture
+def sample_track_dataframe():
+    """Helper function to create a sample GeoPandas GeoDataFrame for gpx file"""
+    coords = [(-122.4, 37.8), (-122.5, 37.7), (-122.6, 37.6)]
+    line = LineString(coords)
+    gdf = gpd.GeoDataFrame(
+        {"name": ["Sample Track"]}, geometry=[line], crs="EPSG:4326"
+    )
+    return gdf
+
+
+@pytest.fixture
+def sample_text():
+    return "Hello, World! \nThis is a sample text file."
+
+
+@pytest.fixture
+def sample_xr_dataset():
+    """Helper function to create a sample xarray Dataset that can be opened as a NetCDF file"""
+    return xr.Dataset(
+        {"foo": (("x", "y"), np.random.rand(4, 5))},
+        coords={
+            "x": [10, 20, 30, 40],
+            "y": pd.date_range("2000-01-01", periods=5),
+            "z": ("x", list("abcd")),
+        },
+    )
---
+++ tests/test_file_udfs.py
@@ -0,0 +1,113 @@
+import os
+import pandas as pd
+import fused
+import geopandas as gpd
+import geopandas.testing as gpd_testing
+import pandas.testing as pd_testing
+import xarray as xr
+import pytest
+from file_udf_helpers import (
+    get_sample_file_from_dataframe,
+    get_sample_file_from_gdf,
+    get_sample_file_from_text,
+    get_sample_netcdf_file,
+)
+
+
+FILES_PATH = os.path.join(os.path.abspath(os.curdir), "files")
+
+
+@pytest.mark.parametrize(
+    "file_format,udf_name",
+    [
+        ("csv", "DuckDB_CSV"),
+        ("json", "DuckDB_JSON"),
+        ("parquet", "DuckDB_Parquet"),
+    ],
+)
+def test_udf_loading_with_duckdb(
+    sample_dataframe: pd.DataFrame, temp_directory: str, file_format: str, udf_name: str
+):
+
+    file_path = get_sample_file_from_dataframe(
+        temp_directory, sample_dataframe, file_format
+    )
+    udf_path = os.path.join(FILES_PATH, udf_name)
+    udf = fused.load(udf_path)
+    result = fused.run(udf, path=file_path, engine="local")
+    pd_testing.assert_frame_equal(result, sample_dataframe)
+
+
+@pytest.mark.parametrize(
+    "file_format,udf_name",
+    [
+        ("geojson", "GeoPandas_File"),
+        ("gpkg", "GeoPandas_File"),
+        ("shp", "GeoPandas_File"),
+        ("zip", "GeoPandas_ZIP"),
+    ],
+)
+def test_loading_with_geopandas(
+    sample_geodataframe: gpd.GeoDataFrame,
+    temp_directory: str,
+    file_format: str,
+    udf_name: str,
+):
+
+    file_path = get_sample_file_from_gdf(
+        temp_directory, sample_geodataframe, file_format
+    )
+    udf = fused.load(os.path.join(FILES_PATH, udf_name))
+    result = fused.run(udf, path=file_path, engine="local")
+    gpd_testing.assert_geodataframe_equal(result, sample_geodataframe)
+
+
+def test_loading_with_geopandas_gpx(
+    sample_track_dataframe: gpd.GeoDataFrame, temp_directory: str
+):
+    file_path = get_sample_file_from_gdf(temp_directory, sample_track_dataframe, "gpx")
+    udf = fused.load(os.path.join(FILES_PATH, "GeoPandas_GPX"))
+    result = fused.run(udf, path=file_path, engine="local")
+    # just check the name and geometry columns
+    print("start")
+    print(result)
+    pd_testing.assert_series_equal(result["name"], sample_track_dataframe["name"])
+    pd_testing.assert_series_equal(result["geometry"], sample_track_dataframe["geometry"])
+
+
+
+def test_udf_loading_with_netcdf(sample_xr_dataset: xr.Dataset, temp_directory: str):
+    # Save the dataset to a NetCDF file
+    file_path = get_sample_netcdf_file(temp_directory, sample_xr_dataset)
+    udf_path = os.path.join(FILES_PATH, "NetCDF_File")
+    udf = fused.load(udf_path)
+    fused.run(udf, path=file_path, engine="local")
+    # just check if no errors
+
+
+@pytest.mark.parametrize(
+    "file_format,udf_name",
+    [
+        ("csv", "Pandas_CSV"),
+        ("xlsx", "Pandas_Excel"),
+        ("parquet", "Pandas_Parquet"),
+    ],
+)
+def test_loading_with_pandas(
+    sample_dataframe: pd.DataFrame, temp_directory: str, file_format: str, udf_name: str
+):
+
+    file_path = get_sample_file_from_dataframe(
+        temp_directory, sample_dataframe, file_format
+    )
+    udf = fused.load(os.path.join(FILES_PATH, udf_name))
+    result = fused.run(udf, path=file_path, engine="local")
+    pd_testing.assert_frame_equal(result, sample_dataframe)
+
+
+def test_loading_with_text(temp_directory: str, sample_text: str):
+
+    file_path = get_sample_file_from_text(temp_directory, sample_text)
+    udf = fused.load(os.path.join(FILES_PATH, "Text_File"))
+    fused.run(udf, path=file_path, engine="local")
+    # just check that the function runs with no errors
