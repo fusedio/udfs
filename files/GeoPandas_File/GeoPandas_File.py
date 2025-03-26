7c0b8ad350a589cda42f25ce047e1308e8d6e73c
Suryashankar Das <suryashankardas.2002@gmail.com>
2025-03-26 16:41:33 +0530

    clean up comments

--- files/GeoPandas_File/GeoPandas_File.py
+++ files/GeoPandas_File/GeoPandas_File.py
@@ -1,17 +1,7 @@
 @fused.udf
 def udf(path: str, preview: bool=False):
     import geopandas as gpd
-    import fsspec
 
-    # extension = path.rsplit(".", maxsplit=1)[-1]
-    # driver = (
-    #     "GPKG"
-    #     if extension == "gpkg"
-    #     else ("ESRI Shapefile" if extension == "shp" else "GeoJSON")
-    # )
-
-    # with fsspec.open(path) as f:
-        # gdf = gpd.read_file(f, driver=driver)
     gdf = gpd.read_file(path)
     print(gdf)
     if preview:
