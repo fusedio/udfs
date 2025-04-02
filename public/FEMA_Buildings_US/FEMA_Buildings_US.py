0df00f54ab29dd5747f65f756bea2972d206a4ff
max@fused.io <max@fused.io>
2025-04-02 09:47:00 +0200

    Update common Github commit hash

--- public/FEMA_Buildings_US/FEMA_Buildings_US.py
+++ public/FEMA_Buildings_US/FEMA_Buildings_US.py
@@ -1,7 +1,7 @@
 @fused.udf
-def udf(bounds: fused.types.Bounds = None):
+def udf(bounds: fused.types.Bounds):
     path='s3://fused-asset/infra/building_oak/'
 
-    utils = fused.load("https://github.com/fusedio/udfs/tree/91845c4/public/common/").utils
+    utils = fused.load("https://github.com/fusedio/udfs/tree/c39f643/public/common/").utils
     df = utils.table_to_tile(bounds, table=path, use_columns=None, min_zoom=13)
     return df
