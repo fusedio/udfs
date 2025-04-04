14f1d4a45bc26dc0100c0ed599d823ffa7130451
Isaac Brodsky <isaac@isaacbrodsky.com>
2025-04-04 07:09:19 -0700

    don't convert bounds back to tile (#792)
    don't convert bounds back to tile
    
    If converting bounds back to tile, the UDF bounds look odd when run in
    Workbench as a Viewport UDF. This way it looks OK as either Viewport UDF
    or Tile UDF.
    
    Made in [Fused Workbench](https://www.fused.io/workbench)

--- public/DuckDB_H3_Example_Tile/DuckDB_H3_Example_Tile.py
+++ public/DuckDB_H3_Example_Tile/DuckDB_H3_Example_Tile.py
@@ -1,37 +1,20 @@
 @fused.udf
-def udf(bounds: fused.types.Bounds=None, resolution: int = 11, min_count: int = 10):
+def udf(bounds: fused.types.Bounds = None, resolution: int = 11, min_count: int = 10):
     import duckdb
     import shapely
     import geopandas as gpd
 
-    # convert bounds to tile
-    common_utils = fused.load("https://github.com/fusedio/udfs/tree/bb712a5/public/common/").utils
-    zoom = common_utils.estimate_zoom(bounds)
-    tile = common_utils.get_tiles(bounds, zoom=zoom)
-
-
-    tile_bounds_gdf = gpd.GeoDataFrame.from_features({"type":"FeatureCollection","features":[{"type":"Feature","properties":{"shape":"Rectangle"},"geometry":{"type":"Polygon","coordinates":[[[-73.99322955922597,40.76627870054801],[-73.96753345042097,40.76627870054801],[-73.96753345042097,40.74825844008337],[-73.99322955922597,40.74825844008337],[-73.99322955922597,40.76627870054801]]]}}]})
-    default_bounds = tile_bounds_gdf.iloc[0].geometry
-    tile_bounds_geom = tile if tile is not None else default_bounds
-
-    tile = tile.bounds.values[0] if tile is not None else default_bounds.bounds
-    print(tile)
-
-    utils = fused.load(
-        "https://github.com/fusedio/udfs/tree/f928ee1/public/common/"
-    ).utils
-    h3_utils = fused.load(
-        "https://github.com/fusedio/udfs/tree/870e162/public/DuckDB_H3_Example/"
-    ).utils
+    utils = fused.load("https://github.com/fusedio/udfs/tree/f928ee1/public/common/").utils
+    h3_utils = fused.load("https://github.com/fusedio/udfs/tree/870e162/public/DuckDB_H3_Example/").utils
     con = duckdb.connect()
 
     h3_utils.load_h3_duckdb(con)
     con.sql(f"""INSTALL httpfs; LOAD httpfs;""")
-    
+
     @fused.cache
     def read_data(url, resolution, min_count, bounds):
         xmin, ymin, xmax, ymax = bounds
-        
+
         query = """
         SELECT h3_h3_to_string(h3_latlng_to_cell(pickup_latitude, pickup_longitude, $resolution)) cell_id,
                h3_cell_to_boundary_wkt(cell_id) boundary,
@@ -48,17 +31,27 @@ def udf(bounds: fused.types.Bounds=None, resolution: int = 11, min_count: int = 
         """
         print(query)
 
-        df = con.sql(query, params={'url': url, 'xmin': xmin, 'xmax': xmax, 'ymin': ymin, "ymax": ymax, 'min_count': min_count, 'resolution': resolution}).df()
+        df = con.sql(
+            query,
+            params={
+                "url": url,
+                "xmin": xmin,
+                "xmax": xmax,
+                "ymin": ymin,
+                "ymax": ymax,
+                "min_count": min_count,
+                "resolution": resolution,
+            },
+        ).df()
         return df
 
-
     df = read_data(
-        url='s3://fused-asset/misc/nyc/tlc/trip-data/yellow_tripdata_2010-01.parquet', 
-        resolution=resolution, 
-        min_count=min_count, 
-        bounds=tile
+        url="s3://fused-asset/misc/nyc/tlc/trip-data/yellow_tripdata_2010-01.parquet",
+        resolution=resolution,
+        min_count=min_count,
+        bounds=bounds,
     )
     print("number of trips:", df.cnt.sum())
-    gdf = gpd.GeoDataFrame(df.drop(columns=['boundary']), geometry=df.boundary.apply(shapely.wkt.loads))
+    gdf = gpd.GeoDataFrame(df.drop(columns=["boundary"]), geometry=df.boundary.apply(shapely.wkt.loads))
     print(gdf)
     return gdf
