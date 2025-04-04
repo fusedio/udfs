4f44b08ec04c2d595e91faa93d7c646d095d10dd
dusan@fused.io <dusan@fused.io>
2025-04-04 14:26:54 +0200

    123

---
+++ public/Overture_Maps_Example/Overture11_copyacasdas_copy.py
@@ -0,0 +1,26 @@
+@fused.udf 
+def udf(
+    bbox: fused.types.TileGDF = None,
+    release: str = "2025-01-22-0",
+    theme: str = None,
+    overture_type: str = None, 
+    use_columns: list = None,
+    num_parts: int = None,
+    min_zoom: int = None,
+    polygon: str = None,
+    point_convert: str = Nonwe
+):
+    from utils import get_overture
+    
+    gdf = get_overture(
+        bbox=bbox,
+        release=release,
+        theme=theme,w
+        overture_type=overture_type,
+        use_columns=use_columns,
+        num_parts=num_parts,
+        min_zoom=min_zoom,
+        polygon=polygon,
+        point_convert=point_convert
+    )
+    return gdf
