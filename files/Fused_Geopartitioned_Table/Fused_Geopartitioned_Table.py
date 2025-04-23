80b7841c0d4f390bd91baa585beafcc1ef12ddb3
Milind Soni <46266943+milind-soni@users.noreply.github.com>
2025-04-24 03:18:08 +0530

    Update Fused_Geopartitioned_Table.py

--- files/Fused_Geopartitioned_Table/Fused_Geopartitioned_Table.py
+++ files/Fused_Geopartitioned_Table/Fused_Geopartitioned_Table.py
@@ -9,5 +9,5 @@ def udf(bounds: fused.types.Bounds, path: str, preview: bool):
         if path.endswith("/_sample") or path.endswith("/_metadata")
         else path
     )
-    df = common.table_to_tile(bounds, table=base_path, use_columns=use_columns , min_zoom = 7)
+    df = common.table_to_tile(bounds, table=base_path, use_columns=use_columns, min_zoom = 7)
     return df
