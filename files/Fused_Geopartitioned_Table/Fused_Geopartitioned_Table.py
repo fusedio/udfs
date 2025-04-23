0cf202e97efd53875a8fd87f7bec02791c86f5c9
Milind Soni <milindsoni@Milinds-MacBook-Air.local>
2025-04-24 03:16:05 +0530

    added changes to the default geopartition udf

--- files/Fused_Geopartitioned_Table/Fused_Geopartitioned_Table.py
+++ files/Fused_Geopartitioned_Table/Fused_Geopartitioned_Table.py
@@ -1,13 +1,13 @@
 @fused.udf
-def udf(bounds: fused.types.Tile, path: str, preview: bool):
+def udf(bounds: fused.types.Bounds, path: str, preview: bool):
     use_columns = ["geometry"] if preview else None
-    utils = fused.load(
-        "https://github.com/fusedio/udfs/tree/2ea46f3/public/common/"
+    common = fused.load(
+        "https://github.com/fusedio/udfs/tree/b41216d/public/common/"
     ).utils
     base_path = (
         path.rsplit("/", maxsplit=1)[0]
         if path.endswith("/_sample") or path.endswith("/_metadata")
         else path
     )
-    df = utils.table_to_tile(bounds, table=base_path, use_columns=use_columns)
+    df = common.table_to_tile(bounds, table=base_path, use_columns=use_columns , min_zoom = 7)
     return df
