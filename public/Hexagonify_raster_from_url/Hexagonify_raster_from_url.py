026421b362ae5b37a446754fb242bb93aacc873a
max@fused.io <max@fused.io>
2025-04-25 14:16:03 -0400

    Hexagonify raster from URL
    Used for documenting raster to Hex docs purposes

---
+++ public/Hexagonify_raster_from_url/Hexagonify_raster_from_url.py
@@ -0,0 +1,38 @@
+@fused.udf
+def udf(raster_path = 'https://s3.amazonaws.com/elevation-tiles-prod/geotiff/4/4/6.tif', 
+        stats_type="mean", 
+        h3_res: int = 4,
+        height_scale: int = 10,
+         color_scale:float=1
+    ):
+
+
+@fused.cache
+def df_to_hex(df, res, latlng_cols=("lat", "lng")):
+    utils = fused.load(
+        "https://github.com/fusedio/udfs/tree/be3bc93/public/common/"
+    ).utils
+    qr = f"""
+            SELECT h3_latlng_to_cell({latlng_cols[0]}, {latlng_cols[1]}, {res}) AS hex, ARRAY_AGG(data) as agg_data
+            FROM df
+            group by 1
+          --  order by 1
+        """
+    con = utils.duckdb_connect()
+    return con.query(qr).df()
+
+
+@fused.cache
+def aggregate_df_hex(df, res, latlng_cols=("lat", "lng"), stats_type="mean"):
+    import pandas as pd
+
+    df = df_to_hex(df, res=res, latlng_cols=latlng_cols)
+    if stats_type == "sum":
+        fn = lambda x: pd.Series(x).sum()
+    elif stats_type == "mean":
+        fn = lambda x: pd.Series(x).mean()
+    else:
+        fn = lambda x: pd.Series(x).mean()
+    df["metric"] = df.agg_data.map(fn)
+    return df
+
