1851f63db760bc61d7f3c438bc83ef0264dbdad3
Suryashankar Das <suryashankardas.2002@gmail.com>
2025-04-28 11:30:46 +0530

    fix Geoparquet_STAC_Land_Productivity public UDF

--- public/Geoparquet_STAC_Land_Productivity/Geoparquet_STAC_Land_Productivity.py
+++ public/Geoparquet_STAC_Land_Productivity/Geoparquet_STAC_Land_Productivity.py
@@ -27,6 +27,9 @@ def udf(
         datetime=f"{year}-01-01T00:00:00.000Z/{year}-12-31T23:59:59.999Z",
     )
 
+    if not item_dicts:
+        return None
+
     # Convert the dictionaries to PySTAC Items
     items = [Item.from_dict(d) for d in item_dicts]
 
