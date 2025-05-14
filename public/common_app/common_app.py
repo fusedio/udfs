6f0117608c79d1ede89027d599ae5fc4415a4d06
sina <sina@fused.io>
2025-05-13 23:45:35 -0400

    refactor & add async_status

--- public/common_app/common_app.py
+++ public/common_app/common_app.py
@@ -4,22 +4,31 @@ def udf():
     return
 
 
-async def async_button(button_label, status_label, st_button=None, st_status=None, end_label=None):
+async def async_status(status_label, end_label=None, st_status=None):
     import streamlit as st
     import asyncio
+
+    if not st_status:
+        st_status = st.empty()
+    with st_status.status(status_label, expanded=False):
+        await asyncio.sleep(0.0025)
+    if end_label:
+        with st_status.status(end_label, expanded=False):
+            pass
+    else:
+        st_status.empty()
+    return True
+
+
+async def async_button(button_label, status_label, end_label=None, st_button=None, st_status=None):
+    import streamlit as st
 
     if not st_button:
         st_button = st.empty()
     if not st_status:
         st_status = st.empty()
     if st_button.button(button_label):
-        with st_status.status(status_label, expanded=False):
-            await asyncio.sleep(0.0025)
-        if end_label:
-            with st_status.status(f'{end_label}', expanded=False):
-                pass
-        else:
-            st_status.empty()
+        await async_status(status_label=status_label, end_label=end_label, st_status=st_status)
         return True
     else:
         return False
