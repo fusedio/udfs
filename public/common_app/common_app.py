daac4f2356c8d7e619b8432d0affc9dce786d40c
Sina Kashuk <sina@nyu.edu>
2025-05-13 21:10:32 -0400

    add async_button (#902)
    add async_button
    
    Made in [Fused Workbench](https://www.fused.io/workbench)

--- public/common_app/common_app.py
+++ public/common_app/common_app.py
@@ -1,16 +1,33 @@
+
 @fused.udf
 def udf():
     """common utils for Fused App. Please do not run this as UDF."""
     return 
 
-def button(label='Run Comparison', spinner='spinner....'):
-    import streamlit as st
-    import time
-    status = st.empty()
-    if st.button(label):
-        with status.status(spinner, expanded=False):
-            time.sleep(0.005)
-        status.empty()
-        return True
-    else:
-        return False
+async def async_button(label, spinner):
+        import streamlit as st    
+        import asyncio
+    
+        status = st.empty()
+        if st.button(label): 
+            with status.status(spinner, expanded=False):
+                await asyncio.sleep(0.0025)
+            status.empty() 
+            return True
+        else:
+            return False
+
+
+def to_sync(fn, *args, _wait_second=0.1, **kwargs):
+    """
+    async def func(x):
+        await asyncio.sleep(x)
+        return x
+    r = to_sync(func, x=2)
+    print(r)
+    """
+    loop = asyncio.get_event_loop()
+    f = loop.run_until_complete(fn(*args, **kwargs))
+    while not f.done():
+        time.sleep(_wait_second)
+    return f.result()
