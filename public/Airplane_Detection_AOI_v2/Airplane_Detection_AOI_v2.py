bc69b083448181a1688e8ea3cda88d83ccdc1aca
max@fused.io <max@fused.io>
2025-05-22 11:51:48 +0200

    Fixing: Airplane detection AOI v2 (using submit, proper bounds & not using utils functions anymore)

--- public/Airplane_Detection_AOI_v2/Airplane_Detection_AOI_v2.py
+++ public/Airplane_Detection_AOI_v2/Airplane_Detection_AOI_v2.py
@@ -2,13 +2,15 @@ import geopandas as gpd
 import json
 
 j = json.dumps({"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[-122.4017963492825,37.628491164736616],[-122.38690611551642,37.606792625414464],[-122.38037214148257,37.60276024740176],[-122.37133040325998,37.61257919769027],[-122.35791086649414,37.60768132596657],[-122.35272358459858,37.61542869072087],[-122.36649356499498,37.62170750731398],[-122.36213655637812,37.628763217050896],[-122.37562464496435,37.63359108358579],[-122.39539369398146,37.63236203450454],[-122.4017963492825,37.628491164736616]]]}}]})
+common = fused.load("https://github.com/fusedio/udfs/tree/36f4e97/public/common/").utils
 
 @fused.udf
 def udf(target_gdf: gpd.GeoDataFrame = j, zoom: int=17):
     import geopandas as gpd
     import pandas as pd
     import json
-    from utils import make_tiles_gdf
+    # from utils import make_tiles_gdf
+    
 
     # Convert to GeoDataFrame, if needed
     if isinstance(target_gdf, str):
@@ -16,7 +18,10 @@ def udf(target_gdf: gpd.GeoDataFrame = j, zoom: int=17):
 
     # Tile the AOI
     bounds = target_gdf.total_bounds
-    gdf_tiles = make_tiles_gdf(bounds, zoom = zoom)
+    # gdf_tiles = make_tiles_gdf(bounds, zoom = zoom)
+    gdf_tiles = common.get_tiles(bounds, zoom=zoom)
+
+    print(gdf_tiles)
 
     # Structure array of tile GeoDataFrames 
     list_of_tile_gdfs = [gpd.GeoDataFrame(row.to_frame().T) for _, row in gdf_tiles.iterrows()]
@@ -24,18 +29,12 @@ def udf(target_gdf: gpd.GeoDataFrame = j, zoom: int=17):
     if len(list_of_tile_gdfs)>400:
         print('Too many ',len(list_of_tile_gdfs), 'tiles, too large a bounds, shrink it until there are less than 400 tiles.')
         return gpd.GeoDataFrame({'a':[1]})
-    
-    # Function to run UDF 
-    @fused.cache
-    def run_udf(bounds):
-        out = fused.run("UDF_DL4EO_Airplane_Detection", bounds=bounds)
-        if len(out)>0:
-            return out
 
-    # Load pinned versions of utility functions.
-    utils = fused.load("https://github.com/fusedio/udfs/tree/ee9bec5/public/common/").utils
     # Run the UDF concurrently for each tile
-    gdfs = utils.run_pool(run_udf, list_of_tile_gdfs)
-    gdf_out = pd.concat(gdfs)
+    gdf_out = fused.submit(
+        "UDF_DL4EO_Airplane_Detection", 
+        [{"bounds": list(bounds.total_bounds)} for bounds in list_of_tile_gdfs],
+        # debug_mode=True
+    )
     print('Total airplanes: ', len(gdf_out))
     return gdf_out
