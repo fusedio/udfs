b11336172c9dbab19d6eab711147b6215c6f8dde
isaac@fused.io <isaac@fused.io>
2025-05-23 13:27:35 -0700

    overture maps example: add clip parameter

--- public/Overture_Maps_Example/Overture_Maps_Example.py
+++ public/Overture_Maps_Example/Overture_Maps_Example.py
@@ -1,17 +1,17 @@
-@fused.udf 
+@fused.udf
 def udf(
-    bounds: fused.types.Bounds = [-0.113,51.503,-0.099,51.513],
+    bounds: fused.types.Bounds = [-0.113, 51.503, -0.099, 51.513],
     release: str = "2025-05-21-0",
     theme: str = None,
-    overture_type: str = None, 
+    overture_type: str = None,
     use_columns: list = None,
     num_parts: int = None,
     min_zoom: int = None,
-    polygon: str = None,
-    point_convert: str = None
+    clip: bool = None,
+    point_convert: str = None,
 ):
     from utils import get_overture
-    
+
     gdf = get_overture(
         bounds=bounds,
         release=release,
@@ -20,7 +20,7 @@ def udf(
         use_columns=use_columns,
         num_parts=num_parts,
         min_zoom=min_zoom,
-        polygon=polygon,
-        point_convert=point_convert
+        clip=clip,
+        point_convert=point_convert,
     )
     return gdf
