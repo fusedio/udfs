0bea25e1e01ff6fa246ae60e24f9995e2979747e
Maxime Lenormand <maxime@mindsbehindmaps.com>
2025-05-27 16:27:35 +0200

    Creating Parquet metadata File UDF

---
+++ files/Metadata_Parquet/Metadata_Parquet.py
@@ -0,0 +1,23 @@
+@fused.udf
+def udf(
+    path: str,
+):
+    import pyarrow.parquet as pq
+    import s3fs
+
+    # Create S3 filesystem object
+    fs = s3fs.S3FileSystem()
+    parquet_file = pq.ParquetFile(fs.open(path))
+    
+    print("Schema:")
+    print(parquet_file.schema)
+    
+    column_names = parquet_file.schema.names
+    print("\nColumn names:")
+    print(column_names)
+    
+    first_rowgroup = parquet_file.read_row_group(0)
+    df = first_rowgroup.to_pandas()
+    
+    print("\nFirst 5 rows:")
+    print(df.head(5).T)
