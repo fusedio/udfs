78c789e0b19a2dae964e20d5e8ab7aafa48be140
aaryan@fused.io <aaryan@fused.io>
2025-06-11 12:08:23 +0000

    123

--- community/aaryan/pink_panda/pink_panda.py
+++ community/aaryan/pink_panda/pink_panda.py
@@ -4,7 +4,7 @@ def udf(bounds: fused.types.Bounds = None, buffer_multiple: float = 1):
     # Using common fused functions as helper
     common = fused.load("https://github.com/fusedio/udfs/tree/bcbb309/public/common/").utils
     # This helper function turns our bounds into XYZ tiles
-    tiles = common.get_tiles(bounds, target_num_tiles=16)
+    tiles = common.get_tiles(bounds, target_num_tiles=17)
     # Buffering tiles internally
     tiles.geometry = tiles.buffer(buffer_multiple / (tiles.z.iloc[0]) ** 2)
     # Use print statements to debug
