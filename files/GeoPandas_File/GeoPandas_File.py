4a8ba5f175446041ca7659b365c0e86eeff40ad5
Maxime Lenormand <maxime@mindsbehindmaps.com>
2025-06-16 11:11:55 +0200

    Adding fused cache to geojson file system

--- files/GeoPandas_File/GeoPandas_File.py
+++ files/GeoPandas_File/GeoPandas_File.py
@@ -2,8 +2,14 @@
 def udf(path: str, preview: bool=False):
     import geopandas as gpd
 
-    gdf = gpd.read_file(path)
-    print(gdf)
+    @fused.cache
+    def get_geojson(path):
+        # Caching is used to prevent doing many calls to the file system
+        return gpd.read_file(path)
+
+    gdf = get_geojson(path)
+    
+    print(gdf.tile)
     if preview:
         return gdf.geometry
     return gdf
