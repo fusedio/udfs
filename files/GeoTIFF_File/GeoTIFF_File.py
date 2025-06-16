75b502c04ef90bb2f7418a0ac1e359cd8e43d40b
Maxime Lenormand <maxime@mindsbehindmaps.com>
2025-06-16 11:05:53 +0200

    Using bounds + update commit in Geotiff_File

--- files/GeoTIFF_File/GeoTIFF_File.py
+++ files/GeoTIFF_File/GeoTIFF_File.py
@@ -1,17 +1,13 @@
 @fused.udf
-def udf(bounds: fused.types.Tile, path: str, *, chip_len=256):
+def udf(bounds: fused.types.Bounds, path: str, *, chip_len=256):
     import numpy as np
-
-    utils = fused.load("https://github.com/fusedio/udfs/tree/a18669/public/common/").utils
-
+    common = fused.load('https://github.com/fusedio/udfs/tree/36f4e97/public/common/').utils
+    tile = common.get_tiles(bounds, target_num_tiles = 4)
+    
     try:
-        arr, metadata = utils.read_tiff(bounds, path, output_shape=(chip_len, chip_len), return_colormap=True , resampling = 'nearest')
+        arr, metadata = common.read_tiff(tile, path, output_shape=(chip_len, chip_len), return_colormap=True, resampling='nearest')
         colormap = metadata['colormap']
-        colored_array = (
-            np.array([colormap[value] for value in arr.flat], dtype=np.uint8)
-            .reshape(arr.shape + (4,))
-            .transpose(2, 0, 1)
-        )
+        colored_array = np.array([colormap[value] for value in arr.flat], dtype=np.uint8).reshape(arr.shape + (4,)).transpose(2, 0, 1)
         return colored_array
     except:
-        return utils.read_tiff(bounds, path, output_shape=(chip_len, chip_len))
+        return common.read_tiff(tile, path, output_shape=(chip_len, chip_len))
