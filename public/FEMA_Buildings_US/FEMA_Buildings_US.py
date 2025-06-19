0dbd49ce486a508ee2437892f5d020a9b8efeaf2
Suryashankar Das <suryashankardas.2002@gmail.com>
2025-06-19 13:27:22 +0530

    update minimum zoom for fema_buildings_us udf

--- public/FEMA_Buildings_US/FEMA_Buildings_US.py
+++ public/FEMA_Buildings_US/FEMA_Buildings_US.py
@@ -3,5 +3,5 @@ def udf(bounds: fused.types.Bounds = [-74.008,40.684,-73.971,40.713]):
     path='s3://fused-asset/infra/building_oak/'
 
     utils = fused.load("https://github.com/fusedio/udfs/tree/d0e8eb0/public/common/").utils
-    df = utils.table_to_tile(bounds, table=path, use_columns=None, min_zoom=13)
+    df = utils.table_to_tile(bounds, table=path, use_columns=None, min_zoom=5)
     return df
