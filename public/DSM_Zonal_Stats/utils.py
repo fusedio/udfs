5a3f16bb72a1d927252d2fc99a4f9d2e11ddc4c4
aaryan@fused.io <aaryan@fused.io>
2025-07-10 16:29:48 +0000

    push

--- public/DSM_Zonal_Stats/utils.py
+++ public/DSM_Zonal_Stats/utils.py
@@ -1,8 +1,11 @@
 import fused
 
-common_utils = fused.load("https://github.com/fusedio/udfs/tree/d0e8eb0/public/common/").utils
-table_to_tile = common_utils.table_to_tile
-geom_stats = common_utils.geom_stats
+table_to_tile = fused.load(
+    "https://github.com/fusedio/udfs/tree/eda5aec/public/common/"
+).utils.table_to_tile
+geom_stats = fused.load(
+    "https://github.com/fusedio/udfs/tree/eda5aec/public/common/"
+).utils.geom_stats
 dsm_to_tile = fused.load(
-    "https://github.com/fusedio/udfs/tree/91845c4/public/DSM_JAXA_Example"
+    "https://github.com/fusedio/udfs/tree/eda5aec/public/DSM_JAXA_Example"
 ).utils.dsm_to_tile
