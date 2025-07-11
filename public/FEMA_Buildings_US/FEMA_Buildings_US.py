b66cc5280c588c93567cf0158c1ee6483a4e6230
isaac@fused.io <isaac@fused.io>
2025-07-11 15:40:56 -0700

    test

--- public/FEMA_Buildings_US/FEMA_Buildings_US.py
+++ public/FEMA_Buildings_US/FEMA_Buildings_US.py
@@ -4,4 +4,4 @@ def udf(bounds: fused.types.Bounds = [-74.008,40.684,-73.971,40.713]):
 
     utils = fused.load("https://github.com/fusedio/udfs/tree/d0e8eb0/public/common/").utils
     df = utils.table_to_tile(bounds, table=path, use_columns=None, min_zoom=13)
-    return df
+    return df# test
