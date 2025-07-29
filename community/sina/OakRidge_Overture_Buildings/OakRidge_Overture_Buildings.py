1d4fabb5d1ee18379b18cf81bb2eda162ab75363
Maxime Lenormand <44133329+MaxLenormand@users.noreply.github.com>
2025-07-29 16:06:02 +0200

    Update OakRidge_Overture_Buildings.py (#1058)

--- community/sina/OakRidge_Overture_Buildings/OakRidge_Overture_Buildings.py
+++ community/sina/OakRidge_Overture_Buildings/OakRidge_Overture_Buildings.py
@@ -2,17 +2,17 @@
 def udf(bounds: fused.types.Bounds = [-122.432,37.800,-122.430,37.802]):
     import geopandas as gpd
     # Load pinned versions of utility functions.
-    common_utils = fused.load("https://github.com/fusedio/udfs/tree/2f41ae1/public/common/").utils
-    overture_utils = fused.load("https://github.com/fusedio/udfs/tree/ee9bec5/public/Overture_Maps_Example/").utils # Load pinned versions of utility functions.
+    common = fused.load("https://github.com/fusedio/udfs/tree/b7637ee/public/common/")
+    overture = fused.load("https://github.com/fusedio/udfs/tree/b7637ee/public/Overture_Maps_Example/")
 
     # Convert bounds to tile
-    tile = common_utils.get_tiles(bounds, clip=True)
+    tile = common.get_tiles(bounds, clip=True)
 
     # 1. Load Overture Buildings
-    gdf_overture = overture_utils.get_overture(bbox=tile)
+    gdf_overture = overture.get_overture(bounds=bounds)
 
     # 2. Load Oak Ridge Buildings
-    gdf_oakridge = common_utils.table_to_tile(
+    gdf_oakridge = common.table_to_tile(
         tile, table="s3://fused-asset/infra/building_oak_states/state=ca/", min_zoom=10
     )
 
@@ -31,7 +31,7 @@ def udf(bounds: fused.types.Bounds = [-122.432,37.800,-122.430,37.802]):
     print(f"IOU: {iou}")
 
     # 6. Overture sources
-    gdf_overture["source"] = gdf_overture["sources"].apply(lambda x: x[0]["dataset"])
+    gdf_overture["source"] = "Overture"
 
     # return intersection
     return gdf_overture[["geometry", "source"]]
