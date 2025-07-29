6449acded1e561ed54b1e2cd1d7f322d275dc585
Milind Soni <milindsoni@ip-192-168-1-5.us-west-2.compute.internal>
2025-07-29 19:01:48 +0530

    updating the common import

--- community/sina/Arcgis_Rgb/Arcgis_Rgb.py
+++ community/sina/Arcgis_Rgb/Arcgis_Rgb.py
@@ -1,9 +1,9 @@
 @fused.udf
 def udf(bounds: fused.types.Bounds = [5.494,46.050,15.443,56.132]):
     # Load pinned versions of utility functions.
-    utils = fused.load("https://github.com/fusedio/udfs/tree/2f41ae1/public/common/").utils
-    bounds = utils.get_tiles(bounds, clip=True)
+    common = fused.load("https://github.com/fusedio/udfs/tree/364f5dd/public/common/")
+    bounds = common.get_tiles(bounds, clip=True)
     # Get the bounding box coordinates
     x, y, z = bounds[["x", "y", "z"]].iloc[0]
     # ArcGIS Online World Imagery basemap
-    return utils.url_to_arr(f"https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}")
+    return common.url_to_arr(f"https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}")
