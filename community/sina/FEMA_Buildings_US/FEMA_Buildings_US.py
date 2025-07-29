7a00877f2c7a224453e9c0121dadb89fdc07ec2b
Milind Soni <milindsoni@ip-192-168-1-5.us-west-2.compute.internal>
2025-07-29 22:12:10 +0530

    updated satellite greenest pixel and Fema

--- community/sina/FEMA_Buildings_US/FEMA_Buildings_US.py
+++ community/sina/FEMA_Buildings_US/FEMA_Buildings_US.py
@@ -2,6 +2,6 @@
 def udf(bounds: fused.types.Bounds = [-74.008,40.684,-73.971,40.713]):
     path='s3://fused-asset/infra/building_oak/'
 
-    utils = fused.load("https://github.com/fusedio/udfs/tree/d0e8eb0/public/common/").utils
-    df = utils.table_to_tile(bounds, table=path, use_columns=None, min_zoom=13)
+    common = fused.load("https://github.com/fusedio/udfs/tree/b7637ee/public/common/")
+    df = common.table_to_tile(bounds, table=path, use_columns=None, min_zoom=13)
     return df
