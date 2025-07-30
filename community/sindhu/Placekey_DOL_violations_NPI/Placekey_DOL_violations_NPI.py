95aa9085e9e8c578945c95525cfc24e84bd3e147
Milind Soni <46266943+milind-soni@users.noreply.github.com>
2025-07-30 15:07:22 +0530

    added community fix (#1082)
    Co-authored-by: Milind Soni <milindsoni@Milinds-MacBook-Air.local>

--- community/sindhu/Placekey_DOL_violations_NPI/Placekey_DOL_violations_NPI.py
+++ community/sindhu/Placekey_DOL_violations_NPI/Placekey_DOL_violations_NPI.py
@@ -1,3 +1,18 @@
+@fused.udf
+def udf(bounds: fused.types.Bounds, preview: bool=False):
+    #import necessary things
+    import pandas as pd
+    import geopandas as gpd
+    import shapely
+
+    #get two placekey'd datasets; choose these based on what's available on the module tab
+    df1 = get_placekeyd_dataset(bounds, "national-provider-identifier")
+    df2 = get_placekeyd_dataset(bounds, "department-of-labor-wage-and-hour-compliance")
+
+    #join the datasets on placekey
+    return placekey_merge(df1, df2)
+
+
 @fused.cache
 def placekey_df_validate(df):
     required_columns = ['placekey', 'address_placekey', 'building_placekey', 'geometry']
@@ -6,15 +21,15 @@ def placekey_df_validate(df):
             raise ValueError("Dataset doesn't have " + column + ".")
 
 @fused.cache
-def get_placekeyd_dataset(bbox, name: str):
-    utils = fused.load('https://github.com/fusedio/udfs/tree/19b5240/public/common/').utils
+def get_placekeyd_dataset(bounds, name: str):
+    common = fused.load("https://github.com/fusedio/udfs/tree/b7637ee/public/common/")
     # pick one of these datasets!!
     datasets = set(['dc-healthy-corner-stores','home-health-agency-medicare-enrollments', 'home-infusion-therapy-provider-medicare-enrollments', 'hospice-medicare-enrollments', 'hospital-medicare-enrollments', 'national-downloadable-files-from-the-doctors-and-clinicians-data-section', 'skilled-nursing-facility-medicare-enrollments', 'rural-health-clinic-medicare-enrollments',
                    'national-provider-identifier', 'department-of-labor-wage-and-hour-compliance'])
     if name in datasets:
         path = 's3://placekey-free-datasets/'+name+'/fused/_sample'
         base_path = path.rsplit('/', maxsplit=1)[0] if path.endswith('/_sample') or path.endswith('/_metadata') else path
-        return utils.table_to_tile(bbox, table=base_path, use_columns=None)
+        return common.table_to_tile(bounds, table=base_path, use_columns=None)
     else:
         raise ValueError(name + " is either not available through Placekey or is not onboarded onto fused.io. Contact Placekey if you would like it to be! https://www.placekey.io/contact-sales")    
 
--- community/sindhu/Placekey_HomeHealthAgency_MedicareEnrollments_x_NPI/Placekey_HomeHealthAgency_MedicareEnrollments_x_NPI.py
+++ community/sindhu/Placekey_HomeHealthAgency_MedicareEnrollments_x_NPI/Placekey_HomeHealthAgency_MedicareEnrollments_x_NPI.py
@@ -1,3 +1,18 @@
+@fused.udf
+def udf(bbox: fused.types.Tile, preview: bool=False):
+    #import necessary things
+    import pandas as pd
+    import geopandas as gpd
+    import shapely
+
+    #get two placekey'd datasets; choose these based on what's available on the module tab
+    df1 = get_placekeyd_dataset(bbox, "national-provider-identifier")
+    df2 = get_placekeyd_dataset(bbox, "home-health-agency-medicare-enrollments")
+
+    #join the datasets on placekey
+    return placekey_merge(df1, df2)
+
+
 @fused.cache
 def placekey_df_validate(df):
     required_columns = ['placekey', 'address_placekey', 'building_placekey', 'geometry']
