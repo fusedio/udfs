62c57b5e932c817b8c0364bfe1b6273c1e8a9e38
Milind Soni <46266943+milind-soni@users.noreply.github.com>
2025-07-31 22:58:25 +0530

    update (#1135)
    update
    
    Made in [Fused Workbench](https://www.fused.io/workbench)

--- community/sina/Meta_CHM_tiles_geojson/Meta_CHM_tiles_geojson.py
+++ community/sina/Meta_CHM_tiles_geojson/Meta_CHM_tiles_geojson.py
@@ -1,6 +1,6 @@
 @fused.udf
 def udf(
-    bounds: fused.types.Bounds = None,
+    bounds: fused.types.Bounds = [-91.54960050811873,17.14190302272142,-70.708790227521,35.49139362643334],
     use_centroid_method: bool = True,
     path: str = "https://dataforgood-fb-data.s3.amazonaws.com/forests/v1/alsgedi_global_v6_float/tiles.geojson",
 ):
