f4ca371ca335c1c3512a2e6d3e9253317ca5e8d0
Milind Soni <46266943+milind-soni@users.noreply.github.com>
2025-07-31 22:20:04 +0530

    single param added (#1131)
    Co-authored-by: Milind Soni <milindsoni@Milinds-MacBook-Air.local>

--- community/sina/DSM_Zonal_Stats/DSM_Zonal_Stats.py
+++ community/sina/DSM_Zonal_Stats/DSM_Zonal_Stats.py
@@ -1,6 +1,6 @@
 @fused.udf
 def udf(
-    bounds: fused.types.Bounds= [-122.416,37.773,-122.383,37.792], min_zoom=15, table="s3://fused-asset/infra/building_msft_us/", chip_len=256
+    bounds: fused.types.Bounds= [-122.44263499875585,37.77313718413549,-122.37559333344419,37.81746423467426], min_zoom=10, table="s3://fused-asset/infra/building_msft_us/", chip_len=256
 ):
     # Load pinned versions of utility functions.
     common = fused.load("https://github.com/fusedio/udfs/tree/c682e4e/public/common/")
@@ -12,6 +12,8 @@ def udf(
         gdf = common.table_to_tile(bounds, table, min_zoom)
         arr = dsm_jaxa.dsm_to_tile(bounds, z_levels=[4, 6, 9, 11], verbose=False)
         gdf_zonal = common.geom_stats(gdf, arr, output_shape=(chip_len, chip_len))
+        bounds_gdf = common.to_gdf(bounds)
+        gdf_zonal = gdf_zonal.clip(bounds_gdf.geometry[0]).explode()
         return gdf_zonal
     else:
         print("Please zoom more... (US Only)")
--- community/sina/FEMA_Buildings_US/FEMA_Buildings_US.py
+++ community/sina/FEMA_Buildings_US/FEMA_Buildings_US.py
@@ -3,5 +3,7 @@ def udf(bounds: fused.types.Bounds = [-74.008,40.684,-73.971,40.713]):
     path='s3://fused-asset/infra/building_oak/'
 
     common = fused.load("https://github.com/fusedio/udfs/tree/b7637ee/public/common/")
-    df = common.table_to_tile(bounds, table=path, use_columns=None, min_zoom=13)
-    return df
+    df = common.table_to_tile(bounds, table=path, use_columns=None, min_zoom=10)
+    bounds_gdf = common.to_gdf(bounds)
+    df = df.clip(bounds_gdf.geometry[0]).explode()
+    return df 
