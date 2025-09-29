acf75e0860a7f3571ed9d837708b54acd40269c1
Milind Soni <46266943+milind-soni@users.noreply.github.com>
2025-09-29 16:11:20 +0530

    Update CDL_Data_Filtering (#1321)
    Update CDL_Data_Filtering
    
    Made in [Fused Workbench](https://www.fused.io/workbench)

--- community/milind/CDL_Data_Filtering/CDL_Data_Filtering.py
+++ community/milind/CDL_Data_Filtering/CDL_Data_Filtering.py
@@ -1,11 +1,13 @@
+common = fused.load("https://github.com/fusedio/udfs/tree/c576dc9/public/common/")
+
 @fused.udf
 def udf(bounds: fused.types.Bounds = [-138.80276917029278,-10.699680369962277,-54.08965045331159,65.89861176695153], buffer_multiple: float = 1, hex_res: int = 4):
     crop_type=''
     
     import pandas as pd
     path = "s3://fused-users/fused/asset/CDL_h12k1p1/year=2024/"
-    H3_From_To_Pos = fused.load("https://github.com/fusedlabs/fusedudfs/tree/5b022e0/H3_From_To_Pos/")
-    df = H3_From_To_Pos.read_hexfile_bounds(bounds=bounds, url=f"{path}overview/hex{hex_res}.parquet", clip=1)
+
+    df = read_hexfile_bounds(bounds=bounds, url=f"{path}overview/hex{hex_res}.parquet", clip=1)
     CDL = fused.load("UDF_CDLs_Tile_Example")
     df = df[df["data"].isin(CDL.crop_type_list(crop_type))]
     import h3.api.basic_int as h3
@@ -52,3 +54,37 @@ def udf(bounds: fused.types.Bounds = [-138.80276917029278,-10.699680369962277,-5
     del df['name']
     print(df)
     return df 
+
+
+def read_hexfile_bounds(bounds:list  = [-84.62623688728367,38.24817244542944,-84.59976376273033,38.27050399126871]
+                        , url: str = "/Users/sinakashuk/Desktop/_hex6", clip=True):
+    common = fused.load("https://github.com/fusedio/udfs/tree/beb4259/public/common/").utils
+    from io import BytesIO
+    import geopandas as gpd
+    import pandas as pd
+    import pyarrow.parquet as pq
+    import base64
+    import shapely
+    with pq.ParquetFile(url) as file:
+        # do not use pq.read_metadata as it may segfault in versions >= 12 (tested on 15.0.1)
+        file_metadata = file.metadata
+        metadata = file_metadata.metadata
+    metadata_bytes = metadata[b"fused:_metadata"]
+    metadata_bytes = base64.decodebytes(metadata_bytes)
+    metadata_bio = BytesIO(metadata_bytes)
+    df = pd.read_parquet(metadata_bio)
+    geoms = shapely.box(
+        df["bbox_minx"], df["bbox_miny"], df["bbox_maxx"], df["bbox_maxy"]
+    )
+    df_meta = gpd.GeoDataFrame(df, geometry=geoms, crs="EPSG:4326")
+    
+    bbox = common.to_gdf(bounds)
+    df_meta = df_meta.sjoin(bbox)
+    chunk_ids = df_meta.chunk_id.values
+    print(chunk_ids)
+    import pyarrow.parquet as pq   
+    table=pq.ParquetFile(url)
+    df = table.read_row_groups(chunk_ids).to_pandas()#, columns=columns).to_pandas()   
+    if clip:
+        return common.filter_hex_bounds(df, bounds)
+    return df
