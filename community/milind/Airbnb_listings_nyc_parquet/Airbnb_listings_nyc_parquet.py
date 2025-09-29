f2c1ad2636fad6e2e891d3fbe1b75bb4a3e69a9d
Milind Soni <46266943+milind-soni@users.noreply.github.com>
2025-09-29 23:00:36 +0530

    Update Airbnb_listings_nyc_parquet (#1335)
    Update Airbnb_listings_nyc_parquet
    
    Made in [Fused Workbench](https://www.fused.io/workbench)

--- community/milind/Airbnb_listings_nyc_parquet/Airbnb_listings_nyc_parquet.py
+++ community/milind/Airbnb_listings_nyc_parquet/Airbnb_listings_nyc_parquet.py
@@ -2,10 +2,25 @@
 def udf(path: str='s3://fused-sample/demo_data/airbnb_listings_nyc.parquet'):
     import geopandas as gpd
     import pandas as pd
+    import numpy as np
+
+    # Load with geopandas if geometry exists, else fallback to pandas
     try:
         df = gpd.read_parquet(path)
-        df = df.to_crs('EPSG:4326')
-    except:
+        try:
+            df = df.to_crs('EPSG:4326')
+        except Exception:
+            pass
+    except Exception:
         df = pd.read_parquet(path)
-    print(df)
-    return df
+
+    # calculating price per person
+    if {"price_in_dollar", "accommodates"}.issubset(df.columns):
+        df["price_per_person"] = np.where(
+            (df["accommodates"] > 0) & df["price_in_dollar"].notna(),
+            df["price_in_dollar"] / df["accommodates"],
+            np.nan
+        )
+
+
+    return df.head()
