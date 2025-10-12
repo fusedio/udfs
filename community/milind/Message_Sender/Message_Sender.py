48f158257e336784704b7350dd9a5c780f0f9f67
milind@fused.io <milind@fused.io>
2025-10-12 10:21:28 +0000

    Update Message_Sender, Message_Reciever

--- community/milind/Message_Sender/Message_Sender.py
+++ community/milind/Message_Sender/Message_Sender.py
@@ -3,7 +3,7 @@ import json, fused
 @fused.udf
 def udf(channel: str = "bounds-demo", sender_id: str = "my_udf"):
     common = fused.load("https://github.com/fusedio/udfs/tree/351515e/public/common/")
-    html = button_html(channel, sender_id)
+    html = map_html(channel, sender_id)
     return common.html_to_obj(html)
 
 
@@ -22,7 +22,7 @@ def map_html(
 
 <link href="https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.css" rel="stylesheet">
 <script src="https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/milind-soni/fused-channel@3950646/channel.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/milind-soni/fused-channel@48b6d9a/channel.js"></script>
 
 
 <script>
@@ -47,7 +47,7 @@ def button_html(channel: str = "button-demo", sender_id: str = "button-udf") -> 
     return f"""<!doctype html>
 <meta charset="utf-8">
 <button id="btn" style="font-size:1.2rem;padding:0.6rem 1rem;margin:2rem;">Click me</button>
-<script src="https://cdn.jsdelivr.net/gh/milind-soni/fused-channel@3950646/channel.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/milind-soni/fused-channel@48b6d9a/channel.js"></script>
 <script>
 document.addEventListener('DOMContentLoaded', () => {{
   const btn = document.getElementById('btn');
