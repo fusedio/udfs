62b9a466626ffc328c8c613e173db6eedb2bee8c
Sina Kashuk <sina@nyu.edu>
2025-11-20 21:35:05 -0500

    Update GeoTIFF_File.py (#1567)

--- files/GeoTIFF_File/GeoTIFF_File.py
+++ files/GeoTIFF_File/GeoTIFF_File.py
@@ -1,5 +1,5 @@
 @fused.udf
 def udf(bounds: fused.types.Bounds, path: str, *, chip_len=256):
-    common = fused.load("https://github.com/fusedio/udfs/tree/6b4e5b9/public/common/")    
-    arr = common.read_tiff_safe(bounds, path, chip_len, max_pixel=10**8)
+    common = fused.load("https://github.com/fusedio/udfs/tree/8080427/public/common/")    
+    arr = common.read_tiff_safe(bounds, path, chip_len, max_pixel=10**8, colormap='plasma')
     return arr
