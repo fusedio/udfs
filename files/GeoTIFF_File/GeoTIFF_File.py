9a77ae7fdd923c8a65971b5226f14b5ff02d0aa8
Sina Kashuk <sina@nyu.edu>
2025-11-20 20:33:22 -0500

    Update GeoTIFF_File.py

--- files/GeoTIFF_File/GeoTIFF_File.py
+++ files/GeoTIFF_File/GeoTIFF_File.py
@@ -1,13 +1,5 @@
 @fused.udf
-def udf(bounds: fused.types.Bounds, path: str, *, chip_len=256):
-    import numpy as np
-    common = fused.load('https://github.com/fusedio/udfs/tree/36f4e97/public/common/').utils
-    tile = common.get_tiles(bounds, target_num_tiles = 4, clip=True)
-    
-    try:
-        arr, metadata = common.read_tiff(tile, path, output_shape=(chip_len, chip_len), return_colormap=True, resampling='nearest')
-        colormap = metadata['colormap']
-        colored_array = np.array([colormap[value] for value in arr.flat], dtype=np.uint8).reshape(arr.shape + (4,)).transpose(2, 0, 1)
-        return colored_array, bounds
-    except:
-        return common.read_tiff(tile, path, output_shape=(chip_len, chip_len)), bounds
+def udf(bounds: fused.types.Bounds=[-180, -90, 180, 90], path: str, *, chip_len=256):
+    common = fused.load("https://github.com/fusedio/udfs/tree/6b4e5b9/public/common/")    
+    arr = common.read_tiff_safe(bounds, path, chip_len, max_pixel=10**8)
+    return arr
