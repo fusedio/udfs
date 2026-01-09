9a956efac0ac02e47d02e08a6f7993ed9ee473cf
Sina Kashuk <sina@nyu.edu>
2026-01-08 23:04:54 -0500

    Update DuckDB_Parquet.py

--- files/DuckDB_Parquet/DuckDB_Parquet.py
+++ files/DuckDB_Parquet/DuckDB_Parquet.py
@@ -2,6 +2,6 @@
 def udf(path: str):
     common = fused.load("https://github.com/fusedio/udfs/tree/4dde28e/public/common/")
     con = common.duckdb_connect()
-    df = con.sql(f"SELECT * FROM read_parquet('{path}') limit 100_000").df()
+    df = con.sql(f"SELECT * FROM read_parquet('{path}') limit 1000").df()
     print(df.T) # transpose the dataframe to make data schema more visible to AI 
     return df
