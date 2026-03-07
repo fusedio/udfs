104de933f4edff9e9ed6834402aba8837e17856d
Milind Soni <46266943+milind-soni@users.noreply.github.com>
2026-03-07 22:49:14 +0530

    update era (#1914)

--- community/max/era5_hex_mean_temp_foss4g/era5_hex_mean_temp_foss4g.py
+++ community/max/era5_hex_mean_temp_foss4g/era5_hex_mean_temp_foss4g.py
@@ -2,7 +2,7 @@
 def udf(month='2024-05',
     bounds:list=[-130, 25, -60, 50]
 ):
-    path = f's3://fused-asset/data/era5/t2m_daily_mean_v2_1000/month={month}/0.parquet'
+    path = f's3://fused-asset/data/era5/t2m_daily_mean_v4_1000/month={month}/0.parquet'
     
     common = fused.load("https://github.com/fusedio/udfs/tree/3991434/public/common/")
     con = common.duckdb_connect() 
