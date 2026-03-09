82973313389241c8745f9171e925d8c7a6096b6e
milind@fused.io <milind@fused.io>
2026-03-09 17:04:48 +0000

    Update map_utils

--- community/milind/map_utils/map_utils.py
+++ community/milind/map_utils/map_utils.py
@@ -56,7 +56,7 @@ DEFAULT_RASTER_STYLE = {
 # You can override this per-run via `deckgl_layers(..., fusedmaps_ref=...)`.
 #
 
-FUSEDMAPS_CDN_REF_DEFAULT = "d7283d7"
+FUSEDMAPS_CDN_REF_DEFAULT = "cd57d84"
 
 def _fusedmaps_cdn_urls(ref: typing.Optional[str] = None) -> tuple[str, str]:
     """Return (js_url, css_url) for a given fusedmaps git ref (commit/tag/branch)."""
