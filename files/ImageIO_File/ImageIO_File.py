d63f4c33d8b29df4d1c6a1acdb022d6a7b669904
Suryashankar Das <suryashankardas.2002@gmail.com>
2026-04-03 18:47:39 +0530

    update imageio file udf

--- files/ImageIO_File/ImageIO_File.py
+++ files/ImageIO_File/ImageIO_File.py
@@ -1,18 +1,17 @@
 @fused.udf
 def udf(path: str, preview: bool):
     import imageio.v3 as iio
-    import s3fs
+    import fsspec
 
-    with s3fs.S3FileSystem().open(path) as f:
+    with fsspec.open(path, "rb") as f:
         im = iio.imread(f)
-        transposed_image = im.transpose(2, 0, 1)
-        print(transposed_image)
+    transposed_image = im.transpose(2, 0, 1)
 
-        if preview:
-            w, h = im.shape[1], im.shape[0]
-            if w > h:
-                return transposed_image, (0, 0, 1, 1 / (w / h))
-            else:
-                return transposed_image, (0, 0, 1 / (h / w), 1)
+    if preview:
+        w, h = im.shape[1], im.shape[0]
+        if w > h:
+            return transposed_image, (0, 0, 1, 1 / (w / h))
+        else:
+            return transposed_image, (0, 0, 1 / (h / w), 1)
 
-        return transposed_image
+    return transposed_image
