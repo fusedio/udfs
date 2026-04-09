3b107aeeb918e47207fa5fb60eb59d4740d2923e
max@fused.io <max@fused.io>
2026-04-09 10:50:53 +0000

    Google_Calendar_routing: adding_loom

---
+++ public/Google_Calendar_routing/loom_demo/loom_demo.py
@@ -0,0 +1,5 @@
+@fused.udf
+def udf():
+    return """
+    <div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/f8ace1d3f9ba4b1eb839dc0011d0edc3" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>
+    """
--- public/Google_Calendar_routing/note_1d9f0a3e_9acf_45d9_bcc2_ddade2e6ab56_7/note_1d9f0a3e_9acf_45d9_bcc2_ddade2e6ab56_7.py
+++ public/Google_Calendar_routing/note_1d9f0a3e_9acf_45d9_bcc2_ddade2e6ab56_7/note_1d9f0a3e_9acf_45d9_bcc2_ddade2e6ab56_7.py
@@ -2,4 +2,4 @@ This UDF:
 
 - **Kept Visible:** AI Agent can visualise it
 - **Added Docstring**: So AI understand what it does
-- **Returns small amount of data**: So AI isn't overloaded with context 
+- **Returns small amount of data**: So AI isn't overloaded with context
