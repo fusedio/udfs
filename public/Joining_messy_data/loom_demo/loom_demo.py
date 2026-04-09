1135acf52ac0dcb6ff30ac32e08555cd7d864296
max@fused.io <max@fused.io>
2026-04-09 10:48:27 +0000

    Joining_messy_data: adding_loom_video

---
+++ public/Joining_messy_data/loom_demo/loom_demo.py
@@ -0,0 +1,5 @@
+@fused.udf
+def udf():
+    return """
+    <div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/d4183e4cfdee42cb835ac793eddd2da3" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>
+    """
