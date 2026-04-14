03fc2e768fb00f31428afb0ac0f3cc47bd762e6f
max@fused.io <max@fused.io>
2026-04-14 18:35:55 +0000

    Connect_your_Canvas_to_Slack: first_version_still_using_openrouter_key

---
+++ public/Connect_your_Canvas_to_Slack/note_920a6a1e_bcdb_4610_88c4_5d05c25dae56_56/note_920a6a1e_bcdb_4610_88c4_5d05c25dae56_56.py
@@ -0,0 +1,4 @@
+## Utils
+
+- Maps each tool visible in OpenAPI into a tool for AI
+- Define AI prompt for answers
---
+++ public/Connect_your_Canvas_to_Slack/note_920a6a1e_bcdb_4610_88c4_5d05c25dae56_58/note_920a6a1e_bcdb_4610_88c4_5d05c25dae56_58.py
@@ -0,0 +1,3 @@
+## Reading Slack channel names
+
+Returns the Slack channel name based on the channel id
