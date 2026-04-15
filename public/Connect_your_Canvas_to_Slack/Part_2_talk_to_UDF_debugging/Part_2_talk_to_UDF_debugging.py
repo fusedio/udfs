551e8375b8a95955e61f0a3421c51fff639c7fed
max@fused.io <max@fused.io>
2026-04-15 09:30:47 +0000

    Connect_your_Canvas_to_Slack: most_recent_videos

--- public/Connect_your_Canvas_to_Slack/Part_2_talk_to_UDF_debugging/Part_2_talk_to_UDF_debugging.py
+++
@@ -1,6 +0,0 @@
-@fused.udf
-def udf():
-    return """
-    <div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/00d91a4976d34b89b75820fdba926b54" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>
-    """
-
