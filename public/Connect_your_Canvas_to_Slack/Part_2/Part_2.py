416ed57ecd404ba713ca0114c0ae1f2b887f9fcc
max@fused.io <max@fused.io>
2026-04-16 08:26:11 +0000

    Connect_your_Canvas_to_Slack: looms_in_widget

---
+++ public/Connect_your_Canvas_to_Slack/Part_2/Part_2.py
@@ -0,0 +1,6 @@
+{
+  "type": "iframe",
+  "props": {
+    "url": "https://www.loom.com/embed/95b35dfe7ad84f66a21a4cfdbb691c08"
+  }
+}
--- public/Connect_your_Canvas_to_Slack/note_1/note_1.py
+++ public/Connect_your_Canvas_to_Slack/note_1/note_1.py
@@ -1,2 +1,1 @@
-
 # Dedicated [Docs](https://docs.fused.io/guide/data-input-outputs/export-api/slack#adding-a-new-canvas-to-slack) page
