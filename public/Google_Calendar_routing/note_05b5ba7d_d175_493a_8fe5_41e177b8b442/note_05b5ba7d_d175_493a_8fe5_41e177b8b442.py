b19d1331b95fa1a06cb78e15771acb24cbcf1378
aman@fused.io <aman@fused.io>
2026-06-09 10:14:55 +0000

    Google_Calendar_routing: checkpointv1

--- public/Google_Calendar_routing/note_05b5ba7d_d175_493a_8fe5_41e177b8b442/note_05b5ba7d_d175_493a_8fe5_41e177b8b442.py
+++ public/Google_Calendar_routing/note_05b5ba7d_d175_493a_8fe5_41e177b8b442/note_05b5ba7d_d175_493a_8fe5_41e177b8b442.py
@@ -1,13 +1,2 @@
-# 1. Google Maps API Requirements
-
-**Get & Manage API Keys:**
-
-- <https://console.cloud.google.com/apis/credentials>
-- Pass API Key in Fused Secrets `google_maps_api`
-
-**Enable APIs:**
-
-- Routes API: <https://console.cloud.google.com/apis/library/routes.googleapis.com>
-- Calendar API: <https://console.cloud.google.com/apis/library/calendar-json.googleapis.com>
-
-# 
+<!--fused:readme-->
+Exported from Fused UDF Workbench
